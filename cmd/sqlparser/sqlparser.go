3a2e3d0f27ecf9d6b0c73a9ef383edd68a989270
Yi Wang <yi.wang.2005@gmail.com>
2018-11-13 14:51:52 +0800

    Update sqlparser.go

--- cmd/sqlparser/sqlparser.go
+++ cmd/sqlparser/sqlparser.go
@@ -1,3 +1,16 @@
+// To checkout this program:
+//
+//   export GOPATH=$HOME/go
+//   go get -u github.com/wangkuiyi/sqlflow/cmd/sqlparser
+//
+// To build after modifying the source code:
+//
+//    go install github.com/wangkuiyi/sqlflow/cmd/sqlparser
+//
+// To run this program:
+//
+//    cat example.sql | $GOPATH/bin/sqlparser
+//
 package main
 
 import (
