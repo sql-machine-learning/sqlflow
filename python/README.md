fbcb2ca7f205bb3d71f107da295afbf8f7ac7d3e
Yang Yang <yzhdoudou@gmail.com>
2018-11-14 12:32:16 -0800

    add json parsing

---
+++ python/README.md
@@ -0,0 +1,14 @@
+# scheduler
+
+## Overview
+
+This package implements a job scheduler in Python, which is used to parse the `json` job description and schedules the ML job using TF. The whole procedure includes
+
+1. Parsing the `json` job description.
+1. Retrieving the data from MySQL via [MySQL Connector Python API](https://dev.mysql.com/downloads/connector/python/). Optionally, retrieving the model from MySQL.
+1. Training the model or predicts using the trained model by calling the user specified TensorFlow estimator.
+1. Writing the trained model or prediction results into a table.
+
+## Example
+
+cat sample.json | python scheduler.py
--- python/example.py
+++ python/example.py
@@ -1,34 +1,48 @@
 import tensorflow as tf
 import database
+import sys, json, os
 
+# TODO(tonyyang-svail): Add make sql recognize the following
+BATCHSIZE = 1
+STEP = 1000
+
+# TODO(tonyyang-svail): 
 USER = "root"
 PASSWORD = "root"
 HOST = "localhost"
 DATABASE = "yang"
 TABLE = "irisis"
-DATA = [("sepal_length", [5.1, 5.0, 6.4]),
-        ("sepal_width", [3.3, 2.3, 2.8]),
-        ("petal_length", [1.7, 3.3, 5.6]),
-        ("petal_width", [0.5, 1.0, 2.2]),
-        ("species", [0, 1, 2])]
 
-# The model consumes all the columns of the table
-# The first n - 1 columns will be X
-# The n th column will be Y
-SQL_COMMAND = "SELECT * FROM {}".format(TABLE)
-MODEL_TYPE = "DNNClassifier"
-HYPERPARAM = {
-        "hidden_units": [10, 10],
-        "n_classes": 3}
-BATCHSIZE = 2
-STEP = 1000
+# DATA = [("sepal_length", [5.1, 5.0, 6.4]),
+#         ("sepal_width", [3.3, 2.3, 2.8]),
+#         ("petal_length", [1.7, 3.3, 5.6]),
+#         ("petal_width", [0.5, 1.0, 2.2]),
+#         ("species", [0, 1, 2])]
+# database.create_table(USER, PASSWORD, HOST, DATABASE, TABLE, DATA)
 
-database.create_table(USER, PASSWORD, HOST, DATABASE, TABLE, DATA)
+def parse_job_desc(json_input):
+    data = json.load(sys.stdin)
+    assert(data["extended"])
+    assert(data["train"])
+
+    sql_command = data["standardSelect"]
+    model_type = data["trainClause"]["estimator"]
+    hyperparam = { x : eval(data["trainClause"]["attrs"][x]) for x in data["trainClause"]["attrs"] }
+    model_dir = os.path.join(data["trainClause"]["save"])
+
+    assert(model_type == "DNNClassifier")
+    assert(isinstance(hyperparam["n_classes"], int))
+    assert(isinstance(hyperparam["hidden_units"], list))
+    return sql_command, model_type, hyperparam, model_dir
+
+SQL_COMMAND, MODEL_TYPE, HYPERPARAM, MODEL_DIR = parse_job_desc(sys.stdin)
+
 field_names, columns = database.load_data(USER, PASSWORD, HOST, DATABASE, SQL_COMMAND)
 
 my_feature_columns = [tf.feature_column.numeric_column(key=key) for key in field_names[:-1]]
 classifier = tf.estimator.DNNClassifier(
         feature_columns=my_feature_columns,
+        model_dir=MODEL_DIR,
         **HYPERPARAM)
 
 train_x = {field_names[i]: columns[i] for i in range(len(field_names) - 1)}
