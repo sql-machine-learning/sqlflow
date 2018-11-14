83e7d9bb36504eec58f563559e33de3387120a6d
Yi Wang <yi.w@antfin.com>
2018-11-14 09:05:10 +0800

    Move testdata into testdata/

--- cmd/sqlparser/sqlparser_test.bash
+++ cmd/sqlparser/sqlparser_test.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cmp --silent \
-    <(cat train_test.sql | go run sqlparser.go) \
-    <(cat train_test.json) \
+    <(cat testdata/train_test.sql | go run sqlparser.go) \
+    <(cat testdata/train_test.json) \
     || (echo "JSONs are different"; exit -1)
