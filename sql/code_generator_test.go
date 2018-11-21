68f724e3e5d1d47408f03eb18cf648be9af0ec25
Yang Yang <yzhdoudou@gmail.com>
2018-11-20 16:48:54 -0800

    connect verifier and codegen. pass train clause

--- sql/code_generator_test.go
+++ sql/code_generator_test.go
@@ -2,25 +2,40 @@ package sql
 
 import (
 	"bytes"
+	"database/sql"
 	"fmt"
+	"github.com/go-sql-driver/mysql"
 	"log"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func init() {
+	testCfg = &mysql.Config{
+		User:   "root",
+		Passwd: "root",
+		Addr:   "localhost:3306",
+	}
+	db, e := sql.Open("mysql", testCfg.FormatDSN())
+	if e != nil {
+		log.Panicf("verify cannot connect to MySQL: %q", e)
+	}
+	testDB = db
+}
+
 const (
 	simpleSelect = `
-SELECT sepal_length, sepal_width, petal_length, petal_width, species
-FROM irisis
+SELECT MonthlyCharges, TotalCharges
+FROM churn.churn
 `
 	simpleTrainSelect = simpleSelect + `
 TRAIN DNNClassifier
 WITH 
   n_classes = 3,
   hidden_units = [10, 20]
-COLUMN sepal_length, sepal_width, petal_length, petal_width
-LABEL species
+COLUMN MonthlyCharges
+LABEL TotalCharges
 INTO
   my_dnn_model
 ;
@@ -35,28 +50,26 @@ var cfg = connectionConfig{
 	Database: "yang",
 	WorkDir:  "/tmp/"}
 
-var cts = columnTypes{
-	Column:   []columnType{
-			{Name: "sepal_length", Type: "numeric_column"},
-			{Name: "sepal_width", Type: "numeric_column"},
-			{Name: "petal_length", Type: "numeric_column"},
-			{Name: "petal_width", Type: "numeric_column"}},
-	Label:	columnType{Name: "species", Type: "numeric_column"}}
-
 func TestCodeGenTrain(t *testing.T) {
 	assert := assert.New(t)
 	assert.NotPanics(func() {
 		sqlParse(newLexer(simpleTrainSelect))
 	})
 
-	tpl := NewTemplateFiller(&parseResult, cts, cfg)
+	fts, e := verify(&parseResult, testCfg)
+	assert.Nil(e,
+		"Make sure you are running the MySQL server in example/churn.")
+	fmt.Println(fts)
+
+	tpl, ok := NewTemplateFiller(&parseResult, fts, cfg)
+	assert.Equal(true, ok)
+
 	var text bytes.Buffer
 	err := codegen_template.Execute(&text, tpl)
 	if err != nil {
 		log.Println("executing template:", err)
 	}
 	assert.Equal(err, nil)
-	fmt.Println(text.String())
 }
 
 // func TestCodeGenInfer(t *testing.T) {
