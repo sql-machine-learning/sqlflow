28b4fccc8b7a0d229c501507140c0768c54689fc
Yi Wang <yi.wang.2005@gmail.com>
2018-12-12 08:59:38 -0800

    Use bytes.Buffer in model.save/load to separate gob message from tarball (#149)
    * Use bytes.Buffer in model.save/load to separate the gob message from the following tarball
    
    * TrainSlct => TrainSelect

--- sql/model.go
+++ sql/model.go
@@ -10,11 +10,16 @@ import (
 	"github.com/wangkuiyi/sqlfs"
 )
 
+type model struct {
+	workDir     string // We don't expose and gob workDir; instead we tar it.
+	TrainSelect string
+}
+
 // save creates a sqlfs table if it doesn't yet exist, and writes the
 // train select statement into the table, followed by the tar-gzipped
 // SQLFlow working directory, which contains the TensorFlow working
 // directory and the trained TenosrFlow model.
-func save(db *sql.DB, table, cwd, trainSlct string) (e error) {
+func (m *model) save(db *sql.DB, table string) (e error) {
 	sqlfn := fmt.Sprintf("sqlflow_models.%s", table)
 	sqlf, e := sqlfs.Create(db, sqlfn)
 	if e != nil {
@@ -22,11 +27,17 @@ func save(db *sql.DB, table, cwd, trainSlct string) (e error) {
 	}
 	defer sqlf.Close()
 
-	if e := gob.NewEncoder(sqlf).Encode(trainSlct); e != nil {
+	// Use a bytes.Buffer as the gob message container to separate
+	// the message from the following tarball.
+	var buf bytes.Buffer
+	if e := gob.NewEncoder(&buf).Encode(m); e != nil {
 		return fmt.Errorf("model.save: gob-encoding model failed: %v", e)
 	}
+	if _, e := buf.WriteTo(sqlf); e != nil {
+		return fmt.Errorf("model.save: write the buffer failed: %v", e)
+	}
 
-	cmd := exec.Command("tar", "czf", "-", "-C", cwd, ".")
+	cmd := exec.Command("tar", "czf", "-", "-C", m.workDir, ".")
 	cmd.Stdout = sqlf
 	return cmd.Run()
 }
@@ -34,29 +45,24 @@ func save(db *sql.DB, table, cwd, trainSlct string) (e error) {
 // load reads from the given sqlfs table for the train select
 // statement, and untar the SQLFlow working directory, which contains
 // the TenosrFlow model, into directory cwd.
-func load(db *sql.DB, table, cwd string) (trainSlct string, e error) {
+func load(db *sql.DB, table, cwd string) (m *model, e error) {
 	sqlfn := fmt.Sprintf("sqlflow_models.%s", table)
 	sqlf, e := sqlfs.Open(db, sqlfn)
 	if e != nil {
-		return "", fmt.Errorf("Cannot open sqlfs file %s: %v", sqlfn, e)
+		return nil, fmt.Errorf("Cannot open sqlfs file %s: %v", sqlfn, e)
 	}
 	defer sqlf.Close()
 
-	// FIXME(tonyyang-svail): directly decoding from sqlf will cause out of bond
-	// error, but it works fine if we loaded the whole chunk to the bytes.Buffer
-	// then decode from there. More details at
-	// https://github.com/wangkuiyi/sqlflow/issues/122
 	var buf bytes.Buffer
-	_, e = buf.ReadFrom(sqlf)
-	if e != nil {
-		return "", e
+	if _, e := buf.ReadFrom(sqlf); e != nil {
+		return nil, e
 	}
-
-	if e := gob.NewDecoder(&buf).Decode(&trainSlct); e != nil {
-		return "", fmt.Errorf("gob-decoding train select failed: %v", e)
+	m = &model{}
+	if e := gob.NewDecoder(&buf).Decode(m); e != nil {
+		return nil, fmt.Errorf("gob-decoding train select failed: %v", e)
 	}
 
 	cmd := exec.Command("tar", "xzf", "-", "-C", cwd)
 	cmd.Stdin = &buf
-	return trainSlct, cmd.Run()
+	return m, cmd.Run()
 }
