61291d0c47557d23dd61bbbc8ad59ee6fbc7d4ce
Yang Yang(Tony) <29932814+tonyyang-svail@users.noreply.github.com>
2019-01-13 19:48:09 -0800

    avoid using log in test functions (#161)

--- sql/codegen_test.go
+++ sql/codegen_test.go
@@ -3,7 +3,6 @@ package sql
 import (
 	"io"
 	"io/ioutil"
-	"log"
 	"os"
 	"strings"
 	"testing"
@@ -59,11 +58,8 @@ func TestCodeGenTrain(t *testing.T) {
 
 	cmd := tensorflowCmd(cwd)
 	cmd.Stdin = pr
-	o, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Println(err)
-	}
-
+	o, e := cmd.CombinedOutput()
+	a.NoError(e)
 	a.True(strings.Contains(string(o), "Done training"))
 }
 
@@ -92,9 +88,7 @@ func TestCodeGenPredict(t *testing.T) {
 
 	cmd := tensorflowCmd(cwd)
 	cmd.Stdin = pr
-	o, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Println(err)
-	}
+	o, e := cmd.CombinedOutput()
+	a.NoError(e)
 	a.True(strings.Contains(string(o), "Done predicting"))
 }
