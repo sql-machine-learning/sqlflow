9fd2d26ed25b6b2a560b2e11179044e5bf2eefc1
Yang Yang <yzhdoudou@gmail.com>
2019-01-23 11:06:17 -0800

    add server test

--- sqlflowserver_test.go
+++ sqlflowserver_test.go
@@ -1,12 +1,51 @@
 package sqlflowserver
 
 import (
-	"github.com/stretchr/testify/assert"
+	"fmt"
+	"log"
+	"net"
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 )
 
-func TestDummy(t *testing.T) {
+const (
+	testStandardSQL = "SELECT ..."
+	testExtendedSQL = "SELECT ... TRAIN ..."
+)
+
+func TestStandardSQL(t *testing.T) {
 	a := assert.New(t)
 	a.True(true)
 }
 
+func TestExtendedSQL(t *testing.T) {
+	a := assert.New(t)
+	a.True(true)
+}
+
+func startServer() {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+
+	s := grpc.NewServer()
+	RegisterSQLFlowServer(s, &Server{})
+	// Register reflection service on gRPC server.
+	reflection.Register(s)
+	log.Println("Server Started at", listener.Addr().(*net.TCPAddr).Port)
+	if err := s.Serve(listener); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
+
+func init() {
+	go startServer()
+
+	os.Exit()
+}
