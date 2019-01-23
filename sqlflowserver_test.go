a2767b6ef06c6d2f51f3cfb068fb773b337ef880
Yang Yang <yzhdoudou@gmail.com>
2019-01-23 12:34:52 -0800

    add sqlflowserver_test.go

--- sqlflowserver_test.go
+++ sqlflowserver_test.go
@@ -1,11 +1,13 @@
 package sqlflowserver
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
@@ -17,35 +19,50 @@ const (
 	testExtendedSQL = "SELECT ... TRAIN ..."
 )
 
-func TestStandardSQL(t *testing.T) {
+var testServerAddress string
+
+func TestSQL(t *testing.T) {
 	a := assert.New(t)
-	a.True(true)
+	// Set up a connection to the server.
+	conn, err := grpc.Dial(testServerAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewSQLFlowClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	_, err = c.Run(ctx, &RunRequest{Sql: testStandardSQL})
+	a.NoError(err)
+
+	_, err = c.Run(ctx, &RunRequest{Sql: testExtendedSQL})
+	a.NoError(err)
 }
 
-func TestExtendedSQL(t *testing.T) {
-	a := assert.New(t)
-	a.True(true)
-}
-
-func startServer() {
+func startServer(done chan bool) {
 	listener, err := net.Listen("tcp", ":0")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Using port:", listener.Addr().(*net.TCPAddr).Port)
+	testServerAddress = fmt.Sprintf("localhost:%v", listener.Addr().(*net.TCPAddr).Port)
+	done <- true
 
 	s := grpc.NewServer()
+	s.GetServiceInfo()
 	RegisterSQLFlowServer(s, &Server{})
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
-	log.Println("Server Started at", listener.Addr().(*net.TCPAddr).Port)
 	if err := s.Serve(listener); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
 }
 
-func init() {
-	go startServer()
+func TestMain(m *testing.M) {
+	done := make(chan bool, 1)
+	go startServer(done)
+	<-done
 
-	os.Exit()
+	os.Exit(m.Run())
 }
