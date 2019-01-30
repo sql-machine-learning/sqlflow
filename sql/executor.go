c7d74526ac52a079758b29a6e077ce795e4d9fd5
Yang Yang(Tony) <29932814+tonyyang-svail@users.noreply.github.com>
2019-01-30 00:12:42 -0800

    fix 177 (#183)

--- sql/executor.go
+++ sql/executor.go
@@ -13,6 +13,12 @@ import (
 	"github.com/go-sql-driver/mysql"
 )
 
+// Log contains a log string and an error, if not nil, during the execution of runExtendedSQL
+type Log struct {
+	log string
+	err error
+}
+
 // Run executes a SQLFlow statements, either standard or extended
 // - Standard SQL statements like `USE database` returns a success message.
 // - Standard SQL statements like `SELECT ...` returns a table in addition
@@ -24,9 +30,11 @@ func Run(slct string, db *sql.DB, cfg *mysql.Config) (string, error) {
 	if strings.Contains(slctUpper, "TRAIN") || strings.Contains(slctUpper, "PREDICT") {
 		pr, e := newParser().Parse(slct)
 		if e == nil && pr.extended {
-			if e = runExtendedSQL(slct, db, cfg, pr); e != nil {
-				log.Errorf("runExtendedSQL error:%v", e)
-				return "", e
+			for l := range runExtendedSQL(slct, db, cfg, pr) {
+				if l.err != nil {
+					log.Errorf("runExtendedSQL error:%v", e)
+					return "", e
+				}
 			}
 			return "Job success", nil
 		}
@@ -103,52 +111,137 @@ func runStandardSQL(slct string, db *sql.DB) (string, error) {
 	return string(buf.Bytes()), nil
 }
 
-func runExtendedSQL(slct string, db *sql.DB, cfg *mysql.Config, pr *extendedSelect) error {
-	startAt := time.Now()
-	log.Infof("Starting runExtendedSQL:%s", slct)
+func runExtendedSQL(slct string, db *sql.DB, cfg *mysql.Config, pr *extendedSelect) chan Log {
+	c := make(chan Log)
 
-	// NOTE: the temporary directory must be in a host directory
-	// which can be mounted to Docker containers.  If I don't
-	// specify the "/tmp" prefix, ioutil.TempDir would by default
-	// generate a directory in /private/tmp for macOS, which
-	// cannot be mounted by Docker into the container.  For more
-	// detailed, please refer to
-	// https://docs.docker.com/docker-for-mac/osxfs/#namespaces.
-	cwd, e := ioutil.TempDir("/tmp", "sqlflow")
-	if e != nil {
-		return e
-	}
-	defer os.RemoveAll(cwd)
+	go func() {
+		defer close(c)
+		err := func() error {
+			startAt := time.Now()
+			log.Infof("Starting runExtendedSQL:%s", slct)
 
-	if pr.train {
-		e = train(pr, slct, db, cfg, cwd)
-	} else {
-		e = pred(pr, db, cfg, cwd)
-	}
-	log.Infof("runExtendedSQL finished, elapsed:%v", time.Now().Sub(startAt))
-	return e
+			// NOTE: the temporary directory must be in a host directory
+			// which can be mounted to Docker containers.  If I don't
+			// specify the "/tmp" prefix, ioutil.TempDir would by default
+			// generate a directory in /private/tmp for macOS, which
+			// cannot be mounted by Docker into the container.  For more
+			// detailed, please refer to
+			// https://docs.docker.com/docker-for-mac/osxfs/#namespaces.
+			cwd, e := ioutil.TempDir("/tmp", "sqlflow")
+			if e != nil {
+				return e
+			}
+			defer os.RemoveAll(cwd)
+
+			if pr.train {
+				for l := range train(pr, slct, db, cfg, cwd) {
+					c <- l
+				}
+			} else {
+				for l := range pred(pr, db, cfg, cwd) {
+					c <- l
+				}
+			}
+			log.Infof("runExtendedSQL finished, elapsed:%v", time.Now().Sub(startAt))
+			return e
+		}()
+
+		if err != nil {
+			c <- Log{"", err}
+		}
+	}()
+
+	return c
 }
 
-func train(tr *extendedSelect, slct string, db *sql.DB, cfg *mysql.Config, cwd string) error {
-	fts, e := verify(tr, db)
-	if e != nil {
-		return e
-	}
+func train(tr *extendedSelect, slct string, db *sql.DB, cfg *mysql.Config, cwd string) chan Log {
+	c := make(chan Log)
 
-	var program bytes.Buffer
-	if e := genTF(&program, tr, fts, cfg); e != nil {
-		return e
-	}
+	go func() {
+		defer close(c)
 
-	cmd := tensorflowCmd(cwd)
-	cmd.Stdin = &program
-	o, e := cmd.CombinedOutput()
-	if e != nil || !strings.Contains(string(o), "Done training") {
-		return fmt.Errorf("Training failed %v: \n%s", e, o)
-	}
+		err := func() error {
+			fts, e := verify(tr, db)
+			if e != nil {
+				return e
+			}
 
-	m := model{workDir: cwd, TrainSelect: slct}
-	return m.save(db, tr.save)
+			var program bytes.Buffer
+			if e := genTF(&program, tr, fts, cfg); e != nil {
+				return e
+			}
+
+			// TODO(tony): redirect cmd.Stdout and cmd.Stderr to c
+			cmd := tensorflowCmd(cwd)
+			cmd.Stdin = &program
+			o, e := cmd.CombinedOutput()
+			if e != nil || !strings.Contains(string(o), "Done training") {
+				return fmt.Errorf("Training failed %v: \n%s", e, o)
+			}
+
+			m := model{workDir: cwd, TrainSelect: slct}
+			return m.save(db, tr.save)
+		}()
+
+		if err != nil {
+			c <- Log{"", err}
+		}
+	}()
+
+	return c
+}
+
+func pred(pr *extendedSelect, db *sql.DB, cfg *mysql.Config, cwd string) chan Log {
+	c := make(chan Log)
+
+	go func() {
+		defer close(c)
+
+		err := func() error {
+			m, e := load(db, pr.model, cwd)
+			if e != nil {
+				return e
+			}
+
+			// Parse the training SELECT statement used to train
+			// the model for the prediction.
+			tr, e := newParser().Parse(m.TrainSelect)
+			if e != nil {
+				return e
+			}
+
+			if e := verifyColumnNameAndType(tr, pr, db); e != nil {
+				return e
+			}
+
+			if e := createPredictionTable(tr, pr, db); e != nil {
+				return e
+			}
+
+			pr.trainClause = tr.trainClause
+			fts, e := verify(pr, db)
+
+			var buf bytes.Buffer
+			if e := genTF(&buf, pr, fts, cfg); e != nil {
+				return e
+			}
+
+			// TODO(tony): redirect cmd.Stdout and cmd.Stderr to c
+			cmd := tensorflowCmd(cwd)
+			cmd.Stdin = &buf
+			o, e := cmd.CombinedOutput()
+			if e != nil || !strings.Contains(string(o), "Done predicting") {
+				return fmt.Errorf("Prediction failed %v: \n%s", e, o)
+			}
+			return nil
+		}()
+
+		if err != nil {
+			c <- Log{"", err}
+		}
+	}()
+
+	return c
 }
 
 // Create prediction table with appropriate column type.
@@ -185,44 +278,6 @@ func createPredictionTable(trainParsed, predParsed *extendedSelect, db *sql.DB) 
 	createStmt := b.String()
 	if _, e := db.Query(createStmt); e != nil {
 		return fmt.Errorf("failed executing %s: %q", createStmt, e)
-	}
-	return nil
-}
-
-func pred(pr *extendedSelect, db *sql.DB, cfg *mysql.Config, cwd string) error {
-	m, e := load(db, pr.model, cwd)
-	if e != nil {
-		return e
-	}
-
-	// Parse the training SELECT statement used to train
-	// the model for the prediction.
-	tr, e := newParser().Parse(m.TrainSelect)
-	if e != nil {
-		return e
-	}
-
-	if e := verifyColumnNameAndType(tr, pr, db); e != nil {
-		return e
-	}
-
-	if e := createPredictionTable(tr, pr, db); e != nil {
-		return e
-	}
-
-	pr.trainClause = tr.trainClause
-	fts, e := verify(pr, db)
-
-	var buf bytes.Buffer
-	if e := genTF(&buf, pr, fts, cfg); e != nil {
-		return e
-	}
-
-	cmd := tensorflowCmd(cwd)
-	cmd.Stdin = &buf
-	o, e := cmd.CombinedOutput()
-	if e != nil || !strings.Contains(string(o), "Done predicting") {
-		return fmt.Errorf("Prediction failed %v: \n%s", e, o)
 	}
 	return nil
 }
