c34f0499c97199988918eb9dc03d78061c6e7a01
Yi Wang <yi.wang.2005@gmail.com>
2019-02-14 15:28:01 -0800

    Open SQLite

--- sql/sql_engine_test.go
+++ sql/sql_engine_test.go
@@ -5,14 +5,15 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestSQLOpen(t *testing.T) {
-	n := fmt.Sprintf("%d%d", time.UnixTime(), os.Getpid())
+	n := fmt.Sprintf("%d%d", time.Now().Unix(), os.Getpid())
 	db, e := sql.Open("sqlite3", n)
 	assert.NoError(t, e)
-	assert.NotNil(db)
+	assert.NotNil(t, db)
 }
