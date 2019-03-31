e9a0ef0dd4e520f19211535cbe0eef60844f85ce
Yi Wang <yi.wang.2005@gmail.com>
2019-03-30 22:30:05 -0700

    hide ParseDSN as parseDSN

--- gohive/dsn.go
+++ gohive/dsn.go
@@ -5,7 +5,7 @@ import (
 	"regexp"
 )
 
-type Config struct {
+type config struct {
 	User   string
 	Passwd string
 	Addr   string
@@ -18,8 +18,8 @@ var (
 	reUserPasswd = regexp.MustCompile(`([^:@]+)(:[^:@]+)?@`)
 )
 
-// ParseDSN requires DSN names in the format [user[:password]@]addr/dbname.
-func ParseDSN(dsn string) (config *Config, err error) {
+// parseDSN requires DSN names in the format [user[:password]@]addr/dbname.
+func parseDSN(dsn string) (*config, error) {
 	// Please read https://play.golang.org/p/_CSLvl1AxOX before code review.
 	sub := reDSN.FindStringSubmatch(dsn)
 	if len(sub) != 4 {
@@ -29,9 +29,9 @@ func ParseDSN(dsn string) (config *Config, err error) {
 	up := reUserPasswd.FindStringSubmatch(sub[1])
 	if len(up) == 3 {
 		if len(up[2]) > 0 {
-			return &Config{User: up[1], Passwd: up[2][1:], Addr: sub[2], DBName: sub[3]}, nil
+			return &config{User: up[1], Passwd: up[2][1:], Addr: sub[2], DBName: sub[3]}, nil
 		}
-		return &Config{User: up[1], Addr: sub[2], DBName: sub[3]}, nil
+		return &config{User: up[1], Addr: sub[2], DBName: sub[3]}, nil
 	}
-	return &Config{Addr: sub[2], DBName: sub[3]}, nil
+	return &config{Addr: sub[2], DBName: sub[3]}, nil
 }
--- gohive/dsn_test.go
+++ gohive/dsn_test.go
@@ -7,27 +7,27 @@ import (
 )
 
 func TestParseDSN(t *testing.T) {
-	cfg, e := ParseDSN("root:root@127.0.0.1/mnist")
+	cfg, e := parseDSN("root:root@127.0.0.1/mnist")
 	assert.Nil(t, e)
 	assert.Equal(t, cfg.User, "root")
 	assert.Equal(t, cfg.Passwd, "root")
 	assert.Equal(t, cfg.Addr, "127.0.0.1")
 	assert.Equal(t, cfg.DBName, "mnist")
 
-	cfg, e = ParseDSN("root@127.0.0.1/mnist")
+	cfg, e = parseDSN("root@127.0.0.1/mnist")
 	assert.Nil(t, e)
 	assert.Equal(t, cfg.User, "root")
 	assert.Equal(t, cfg.Passwd, "")
 	assert.Equal(t, cfg.Addr, "127.0.0.1")
 	assert.Equal(t, cfg.DBName, "mnist")
 
-	cfg, e = ParseDSN("127.0.0.1/mnist")
+	cfg, e = parseDSN("127.0.0.1/mnist")
 	assert.Nil(t, e)
 	assert.Equal(t, cfg.User, "")
 	assert.Equal(t, cfg.Passwd, "")
 	assert.Equal(t, cfg.Addr, "127.0.0.1")
 	assert.Equal(t, cfg.DBName, "mnist")
 
-	cfg, e = ParseDSN("127.0.0.1")
+	cfg, e = parseDSN("127.0.0.1")
 	assert.NotNil(t, e) // Need database name.
 }
