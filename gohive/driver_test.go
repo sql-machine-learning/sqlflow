55cd54c229fe0e6dab6f8c1e462372ad3b94b1f0
Yi Wang <yi.wang.2005@gmail.com>
2019-04-01 06:51:40 -0700

    Remove unnecesary print outs

--- gohive/driver_test.go
+++ gohive/driver_test.go
@@ -1,38 +1,33 @@
 package gohive
 
-import "testing"
-import "log"
-import "database/sql"
+import (
+	"database/sql"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestOpenConnection(t *testing.T) {
 	db, err := sql.Open("hive", "127.0.0.1:10000")
-	if err != nil {
-		log.Fatal(err)
-	}
+	assert.Nil(t, err)
 	defer db.Close()
 }
 
 func TestQuery(t *testing.T) {
 	db, _ := sql.Open("hive", "127.0.0.1:10000/churn")
-	var (
-		customerid string
-		gender     string
-	)
 	rows, err := db.Query("SELECT customerID, gender FROM churn.train")
-	if err != nil {
-		log.Fatal(err)
-	}
+	assert.Nil(t, err)
 	defer db.Close()
 	defer rows.Close()
+
+	n := 0
+	customerid := ""
+	gender := ""
 	for rows.Next() {
 		err := rows.Scan(&customerid, &gender)
-		if err != nil {
-			log.Fatal(err)
-		}
-		log.Println(customerid, gender)
+		assert.Nil(t, err)
+		n++
 	}
-	err = rows.Err()
-	if err != nil {
-		log.Fatal(err)
-	}
+	assert.Nil(t, rows.Err())
+	assert.Equal(t, 82, n) // The imported data size is 82.
 }
