200b85e2084d38d27f5739b2bb28c68e43f7ab8f
Yang Yang <yzhdoudou@gmail.com>
2019-04-10 17:49:03 -0700

    -x => -e

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-set -x
+set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t sqlflow:nightly -f ./Dockerfile $GOPATH/bin
-docker tag sqlflow:nightly sqlflow/sqlflow:nightly
-docker push sqlflow/sqlflow:nightly
+# docker build -t sqlflow:nightly -f ./Dockerfile $GOPATH/bin
+# docker tag sqlflow:nightly sqlflow/sqlflow:nightly
+# docker push sqlflow/sqlflow:nightly
