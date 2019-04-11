c25d6c96e772c75beb532e28225022c10538dd7d
Yang Yang <yzhdoudou@gmail.com>
2019-04-10 17:49:59 -0700

    bring back docker push

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# docker build -t sqlflow:nightly -f ./Dockerfile $GOPATH/bin
-# docker tag sqlflow:nightly sqlflow/sqlflow:nightly
-# docker push sqlflow/sqlflow:nightly
+docker build -t sqlflow:nightly -f ./Dockerfile $GOPATH/bin
+docker tag sqlflow:nightly sqlflow/sqlflow:nightly
+docker push sqlflow/sqlflow:nightly
