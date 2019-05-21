36987bd7cb2be02e720ccd08357fc051a929c75d
Wu Yi <typhoonzero1986@gmail.com>
2019-05-21 15:25:05 +0800

    remove gocoveralls in deploy.sh since it need more test (#412)
    * remove gocoveralls in deploy.sh since it need more test
    
    * skip hive sqlfs test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,9 +11,3 @@ fi
 
 docker build -t sqlflow/sqlflow:$DOCKER_TAG -f ./Dockerfile .
 docker push sqlflow/sqlflow:$DOCKER_TAG
-
-# install goveralls in here because in travis-ci, we mount $GOPATH directly
-# into docker container which will overwrite goveralls installed in docker image.
-go get golang.org/x/tools/cmd/cover && \
-go get github.com/mattn/goveralls
-/go/bin/goveralls -coverprofile=coverage.out -service=travis-ci -repotoken $COVERALLS_TOKEN
