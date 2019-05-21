9c88abc3e78ecc6ce3d60c776b804abae659d4b0
typhoonzero <typhoonzero1986@gmail.com>
2019-05-21 15:58:39 +0800

    test on ci

--- scripts/test_hive.sh
+++ scripts/test_hive.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-set -e
-. /miniconda/etc/profile.d/conda.sh
-source activate sqlflow-dev
 
-# wait until hive test server is ready
+# Wait until hive test server is ready, port 8899
+# is a status port indicates the hive server container
+# is ready, see .travis.yml for the details
 while [ 1 ]
 do
-nc -z localhost 10000
+nc -z localhost 8899
+curl http://localhost:8899 2>/dev/null
 if [ $? -eq 0 ]; then
 break
 else
@@ -14,6 +14,10 @@ echo "port not ready"
 sleep 1
 fi
 done
+
+set -e
+. /miniconda/etc/profile.d/conda.sh
+source activate sqlflow-dev
 
 go generate ./...
 go get -v -t ./...
