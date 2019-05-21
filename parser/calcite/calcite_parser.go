658ab25c6454f3ea0cb7234a0b0066e6788cfa26
Yi Wang <yi.wang.2005@gmail.com>
2019-05-21 14:35:13 -0700

    Update files

--- parser/calcite/calcite_parser.go
+++ parser/calcite/calcite_parser.go
@@ -4,6 +4,33 @@
 // The server implementation is in CalciteParserServer.java.
 package calcite
 
-func Parse(sql string) (err error, idx int) {
+import (
+	context "context"
+	"flag"
+	"log"
+	"time"
 
+	grpc "google.golang.org/grpc"
+)
+
+var (
+	addr = flag.String("calciate_parser_addr", ":50051", "Listening address of the Calciate parser gRPC server")
+	conn *grpc.ClientConn
+)
+
+func Parse(sql string) (idx int, err error) {
+	conn, err = grpc.Dial(*addr, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := NewCalciteParserClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.Parse(ctx, &CalciteParserRequest{Query: sql})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.Message)
 }
