03a7fea622e5756950f0618b67d88cfcd1a14772
typhoonzero <typhoonzero1986@gmail.com>
2019-05-24 16:46:26 +0800

    follow comments

--- scripts/copyright.py
+++ scripts/copyright.py
@@ -92,17 +92,39 @@ def main(argv=None):
 
     retv = 0
     for filename in args.filenames:
-        print("deal with", filename)
         fd = io.open(filename, encoding="utf-8")
         first_line = fd.readline()
         second_line = fd.readline()
+        third_line = fd.readline()
+        # check for 3 head lines
         if "COPYRIGHT " in first_line.upper(): continue
-        if first_line.startswith("#!") or PYTHON_ENCODE.match(
-                second_line) != None or PYTHON_ENCODE.match(first_line) != None:
-            continue
-        original_contents = io.open(filename, encoding="utf-8").read()
-        new_contents = generate_copyright(
-            COPYRIGHT, lang_type(filename)) + original_contents
+        if "COPYRIGHT " in second_line.upper(): continue
+        if "COPYRIGHT " in third_line.upper(): continue
+        skip_one = False
+        skip_two = True
+        if first_line.startswith("#!"):
+            skip_one = True
+        if PYTHON_ENCODE.match(second_line) != None:
+            skip_two = True
+        if PYTHON_ENCODE.match(first_line) != None:
+            skip_one = True
+
+        original_content_lines = io.open(filename, encoding="utf-8").read().split("\n")
+        copyright_string = generate_copyright(COPYRIGHT, lang_type(filename))
+        if skip_one:
+            new_contents = "\n".join(
+                [original_content_lines[0],
+                copyright_string] + original_content_lines[1:]
+            )
+        elif skip_two:
+            new_contents = "\n".join(
+                [original_content_lines[0],
+                original_content_lines[1],
+                copyright_string] + original_content_lines[2:]
+            )
+        else:
+            new_contents = generate_copyright(
+                COPYRIGHT, lang_type(filename)) + "\n".join(original_content_lines)
         print('Auto Insert Copyright Header {}'.format(filename))
         retv = 1
         with io.open(filename, 'w') as output_file:
