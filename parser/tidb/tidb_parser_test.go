1424350537aaf42ebf86fba10d1a61469937be5a
Yi Wang <yi.wang.2005@gmail.com>
2019-05-27 12:37:01 -0700

    Make tidb.Parser calls TiDB parser twice as Calcite paser does (#450)

--- parser/tidb/tidb_parser_test.go
+++ parser/tidb/tidb_parser_test.go
@@ -21,18 +21,56 @@ import (
 
 func TestTiDBParser(t *testing.T) {
 	a := assert.New(t)
+	var (
+		i int
+		e error
+	)
 
-	i, e := Parse("select * frm tbl") // frm->from
+	Init()
+
+	i, e = Parse("select * frm t1") // frm->from
 	a.NoError(e)
 	a.Equal(9, i)
 
-	i, e = Parse("select * from tbl where id = 1")
+	i, e = Parse("select a frm t1") // frm->from
+	a.NoError(e)
+	a.Equal(13, i) // This seems a bug of TiDB parser.  Should be 9.
+
+	i, e = Parse("select a, b from t1")
 	a.NoError(e)
 	a.Equal(-1, i)
 
-	i, e = Parse("select * from tbl where id = 1 TRAIN DNNClassifier WITH learning_rate=0.1")
+	i, e = Parse("select a b from t1")
 	a.NoError(e)
-	a.Equal(31, i)
+	a.Equal(-1, i)
+
+	i, e = Parse("select a b c from t1") // Same as Calcite parser -- no more than 2 fields without ','.
+	a.NoError(e)
+	a.Equal(11, i)
+
+	i, e = Parse("select * from t1")
+	a.NoError(e)
+	a.Equal(-1, i)
+
+	i, e = Parse("select * from t1, t2")
+	a.NoError(e)
+	a.Equal(-1, i)
+
+	i, e = Parse("select * from t1 t2")
+	a.NoError(e)
+	a.Equal(-1, i)
+
+	i, e = Parse("select * from t1 t2 t3") // Same as Calcite parser -- no more than 2 tables without ",".
+	a.NoError(e)
+	a.Equal(20, i)
+
+	i, e = Parse("select * from t1 where id in (select * from t2)")
+	a.NoError(e)
+	a.Equal(-1, i)
+
+	i, e = Parse("select * from t1 where id in (select * from t2) TRAIN DNNClassifier WITH learning_rate=0.1")
+	a.NoError(e)
+	a.Equal(48, i)
 
 	i, e = Parse("select * from tbl where id = 1 predict tbl.predicted using my_model")
 	a.NoError(e)
