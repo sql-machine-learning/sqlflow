3d70830240c62f5359bc3905517f5f04814a6095
Yang Yang <yzhdoudou@gmail.com>
2019-05-31 15:44:22 +0800

    Hot fix on CI script

--- scripts/test_hive.sh
+++ scripts/test_hive.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+set -e
 
 # Wait until hive test server is ready, port 8899
 # is a status port indicates the hive server container
