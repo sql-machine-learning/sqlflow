1147883480de0bd9ddbbaeaa078a312a9e0eaa07
Yan Xu <yancey1989@gmail.com>
2019-06-03 10:09:22 +0800

    deploy on k8s doc (#475)
    * deploy on k8s doc
    
    * update install doc
    
    * use official docker images
    
    * one pod
    
    * fix typo
    
    * official docker image

---
+++ doc/run_on_kubernetes.md
@@ -0,0 +1,59 @@
+# Running SQLFlow on Kubernetes
+
+This is a tutorial on how to run SQLFlow on Kubernetes, and this tutorial will deploy:
+- MySQL server with the SQLFlow example datasets loaded, and
+- SQLFlow components contain the sqlflow-server the Jupyter notebook.
+
+Then you can run the SQLFlow query in the Jupyter notebook on your web browser.
+
+## Preparation
+
+1. Setup a Kubernetes cluster: You can refer to the [official page](https://kubernetes.io/docs/setup) to set up a 
+full cluster or use a local quick start tool: [minikube](https://kubernetes.io/docs/tasks/tools/install-minikube/)
+This tutorial would use [minikube] to demonstrate the SQLFlow.
+1. [Install the kubectl](https://kubernetes.io/docs/tasks/tools/install-kubectl/), which is the command line tool
+to interact with the Kubernetes cluster.
+1. Pull the official Docker image [sqlflow/sqlflow:latest](https://hub.docker.com/r/sqlflow/sqlflow) or [build
+from source](/doc/build.md)
+
+## Deploy the SQLFlow Components
+
+1. Deploy the MySQL service and sqlflow service in the difference containers in one Pod. 
+    ``` bash
+    > kubectl create -f k8s/sqlflow-mysql.yaml
+    ```
+
+1. Testing your SQLFlow setup
+    You can find a Pod on Kubernetes with the prefix `sqlflow-mysql-*`:
+    ``` bash
+    > kubectl get pods
+    NAME    READY   STATUS    RESTARTS   AGE
+    NAME                             READY   STATUS    RESTARTS   AGE
+    sqlflow-mysql-77f8674899-dv269   2/2     Running   0          75m
+    ```
+    The logs of the two containers similar to:
+    ``` bash
+    > kubectl logs sqlflow-mysql-77f8674899-dv269 mysql
+     * Starting MySQL database server mysqld
+       ...done.
+    > kubectl logs sqlflow-mysql-77f8674899-dv269 sqlflow
+    Connect to the datasource mysql://root:root@tcp(10.100.73.238:3306)/?maxAllowedPacket=0
+    2019/05/30 09:57:55 Server Started at :50051
+    ```
+
+## Running your Query in SQLFlow 
+
+1. Copy the node IP of the sqlflow Pod on minikube as the follows command:
+    ``` bash
+    > minikube ip
+    192.168.99.100
+    ```
+    **NOTE**: If you are using a **real** cluster, you can find the node domain/IP from the `NODE` column
+    using`kubectl get pods -o wide`:
+    ``` bash
+    > kubectl get pods -o wide
+    NAME    READY   STATUS    RESTARTS   AGE   IP   NODE    NOMINATED   NODE    READINESS   GATES
+    sqlflow-mysql-77f8674899-dv269  2/2    Running  0   9s  172.17.0.4  minikube   <none>           <none>
+    ```
+
+1. Open a web browser and go to '<node-ip>:8888', you can find the [SQLFlow example](/example/jupyter/example.ipynb) in the Jupyter notebook file lists.
---
+++ scripts/start.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+SQLFLOW_MYSQL_HOST=${SQLFLOW_MYSQL_HOST:-127.0.0.1}
+SQLFLOW_MYSQL_PORT=${SQLFLOW_MYSQL_PORT:-3306}
+
+function sleep_until_mysql_is_ready() {
+  until mysql -u root -proot --host ${SQLFLOW_MYSQL_HOST} --port ${SQLFLOW_MYSQL_PORT} -e ";" ; do
+    sleep 1
+    read -p "Can't connect, retrying..."
+  done
+}
+
+function setup_mysql() {
+    # Start mysqld
+    sed -i "s/.*bind-address.*/bind-address = ${SQLFLOW_MYSQL_HOST}/" /etc/mysql/mysql.conf.d/mysqld.cnf
+    service mysql start
+    sleep 1
+    sleep_until_mysql_is_ready
+    # FIXME(typhoonzero): should let docker-entrypoint.sh do this work
+    for f in /docker-entrypoint-initdb.d/*; do
+        cat $f | mysql -uroot -proot --host ${SQLFLOW_MYSQL_HOST} --port ${SQLFLOW_MYSQL_PORT}
+    done
+    # Grant all privileges to any remote hosts so that the sqlserver can be scaled into more than one replicas.
+    mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'' IDENTIFIED BY 'root' WITH GRANT OPTION;"
+}
+
+function setup_sqlflow() {
+  sleep_until_mysql_is_ready
+
+  DS="mysql://root:root@tcp(${SQLFLOW_MYSQL_HOST}:${SQLFLOW_MYSQL_PORT})/?maxAllowedPacket=0"
+  echo "Connect to the datasource ${DS}"
+  # Start sqlflowserver
+  sqlflowserver --datasource=${DS} &
+  # Start jupyter notebook
+  SQLFLOW_SERVER=localhost:50051 jupyter notebook --ip=0.0.0.0 --port=8888 --allow-root --NotebookApp.token=''
+}
+
+function print_usage() {
+  echo "Usage: /bin/bash start.sh [OPTION]\n"
+  echo "\tmysql: setup the mysql server with the example dataset initialized."
+  echo "\tsqlflow: setup the sqlflow server and the jupyter notebook which port is 8888"
+  echo "\tall(default): setup mysql server and sqlflow server in one container."
+}
+
+function main() {
+  ARG=${1:-all}
+  case $ARG in 
+    mysql)
+      setup_mysql
+      sleep infinity
+      ;;
+    sqlflow)
+      setup_sqlflow
+      ;;
+    all)
+      echo "setup all-in-one"
+      setup_mysql
+      setup_sqlflow 
+      ;;
+    *)
+      print_usage
+      ;;
+  esac
+}
+
+main $@
