c688f661221b9c39d41786aa9fbf0c955f596850
雄木 <xiongmu.wy@antfin.com>
2019-07-08 19:00:47 +0800

    debug ci

--- sql/codegen.go
+++ sql/codegen.go
@@ -402,9 +402,10 @@ def eval_input_fn(batch_size):
         feature_column_names, "{{.Y.FeatureName}}", feature_metas)
     dataset = tf.data.Dataset.from_generator(gen, (tuple(feature_types), tf.int64))
     ds_mapper = functools.partial(_parse_sparse_feature, feature_metas=feature_metas)
-    dataset = dataset.map(ds_mapper)
+    dataset = dataset.map(ds_mapper).batch(batch_size)
     return dataset
 
+print("######### start running predict")
 pred_dataset = eval_input_fn(1)
 one_batch = pred_dataset.__iter__().next()
 # NOTE: must run predict one batch to initialize parameters
@@ -419,6 +420,7 @@ column_names.append("{{.Y.FeatureName}}")
 while True:
     try:
         features = pred_dataset.get_next()
+        print("###DEBUG: generated features: ", features)
     except tf.errors.OutOfRangeError:
         break
     result = classifier.predict_on_batch(features[0])
