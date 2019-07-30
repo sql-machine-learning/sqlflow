c8750cf36fe3b68da39aba870f5f25fb96b090b9
Yang Yang <yzhdoudou@gmail.com>
2019-07-30 16:51:57 -0700

    fix 621

--- sql/verifier.go
+++ sql/verifier.go
@@ -19,65 +19,22 @@ import (
 	"strings"
 )
 
-// fieldTypes[field][table]type.  For more information, please check
-// verifier_test.go.
-type fieldTypes map[string]map[string]string
+// fieldTypes[field]type.
+type fieldTypes map[string]string
 
 func (ft fieldTypes) String() string {
 	var b bytes.Buffer
-	for field, table := range ft {
-		for t, typ := range table {
-			fmt.Fprintf(&b, "%s, %s, %s\n", field, t, typ)
-		}
+	for field, typ := range ft {
+		fmt.Fprintf(&b, "%s, %s\n", field, typ)
 	}
 	return b.String()
 }
 
-// verify checks the following:
-//
-// 1. The standard SELECT part is syntactically and logically legal.
-//
-// 2. TODO(yi): The COLUMN clause refers to only fields in the SELECT
-//    clause.  Please be aware that both SELECT and COLUMN might have
-//    star '*'.
-//
-// It returns a fieldTypes describing types of fields in SELECT.
-func verify(slct *extendedSelect, db *DB) (ft fieldTypes, e error) {
-	if e := dryRunSelect(slct, db); e != nil {
-		return nil, e
-	}
-	return describeTables(slct, db)
-}
-
-func dryRunSelect(slct *extendedSelect, db *DB) error {
-	oldLimit := slct.standardSelect.limit
-	defer func() {
-		slct.standardSelect.limit = oldLimit
-	}()
-
-	slct.standardSelect.limit = "1"
-	stmt := slct.standardSelect.String()
-	rows, e := db.Query(stmt)
-	if e != nil {
-		return fmt.Errorf("dryRunSelect failed executing %s: %q", stmt, e)
-	}
-	defer rows.Close()
-
-	return rows.Err()
-}
-
 func (ft fieldTypes) get(ident string) (string, bool) {
-	tbl, fld := decomp(ident)
-	tbls, ok := ft[fld]
+	typ, ok := ft[ident]
 	if !ok {
 		return "", false
 	}
-	if len(tbl) == 0 && len(tbls) == 1 {
-		for _, typ := range tbls {
-			return typ, true
-		}
-	}
-	typ, ok := tbls[tbl]
 	return typ, ok
 }
 
@@ -94,71 +51,41 @@ func decomp(ident string) (tbl string, fld string) {
 	return ident[0:idx], ident[idx+1:]
 }
 
-// Retrieve the type of fields mentioned in SELECT.
-func describeTables(slct *extendedSelect, db *DB) (ft fieldTypes, e error) {
-	ft = indexSelectFields(slct)
-	hasStar := len(ft) == 0
-	for _, tn := range slct.tables {
-		// MaxCompute's statement ends with a semicolon
-		q := "SELECT * FROM " + tn + " LIMIT 1;"
-		rows, err := db.Query(q)
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-
-		if !rows.Next() {
-			return nil, fmt.Errorf("table[%s] is empty", tn)
-		}
-
-		if rows.Err() != nil {
-			return nil, e
-		}
-
-		columnTypes, err := rows.ColumnTypes()
-		if err != nil {
-			return nil, err
-		}
-		for _, ct := range columnTypes {
-			_, fld := decomp(ct.Name())
-			typeName := ct.DatabaseTypeName()
-			if hasStar {
-				if _, ok := ft[fld]; !ok {
-					ft[fld] = make(map[string]string)
-				}
-				ft[fld][tn] = typeName
-			} else {
-				if tbls, ok := ft[fld]; ok {
-					if len(tbls) == 0 {
-						tbls[tn] = typeName
-					} else if _, ok := tbls[tn]; ok {
-						tbls[tn] = typeName
-					}
-				}
-			}
-		}
+// verify checks the standard SELECT part is syntactically and logically legal.
+//
+// It returns a fieldTypes describing types of fields in SELECT.
+func verify(slct *extendedSelect, db *DB) (fieldTypes, error) {
+	q := slct.standardSelect.String()
+	rows, err := db.Query(q)
+	if err != nil {
+		return nil, err
 	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		return nil, fmt.Errorf("query %s gives 0 row", q)
+	}
+
+	if rows.Err() != nil {
+		return nil, err
+	}
+
+	columnTypes, err := rows.ColumnTypes()
+	if err != nil {
+		return nil, err
+	}
+
+	ft := make(fieldTypes)
+	for _, ct := range columnTypes {
+		_, fld := decomp(ct.Name())
+		typeName := ct.DatabaseTypeName()
+		if _, ok := ft[fld]; ok {
+			return nil, fmt.Errorf("duplicated field name %s", fld)
+		}
+		ft[fld] = typeName
+	}
+
 	return ft, nil
-}
-
-// Index fields in the SELECT clause.  For `SELECT f`, returns {f:{}}.
-// For `SELECT t.f`, returns {f:{t:1}}.  For `SELECT t1.f, t2.f`,
-// returns {f:{t1:1,t2:1}}.  For `SELECT ... * ...`, returns {}.
-func indexSelectFields(slct *extendedSelect) (ft fieldTypes) {
-	ft = make(fieldTypes)
-	for _, f := range slct.fields.Strings() {
-		if f == "*" {
-			return fieldTypes{}
-		}
-		tbl, fld := decomp(f)
-		if _, ok := ft[fld]; !ok {
-			ft[fld] = make(map[string]string)
-		}
-		if len(tbl) > 0 {
-			ft[fld][tbl] = ""
-		}
-	}
-	return ft
 }
 
 // Check train and pred clause uses has the same feature columns
@@ -173,6 +100,11 @@ func verifyColumnNameAndType(trainParsed, predParsed *extendedSelect, db *DB) er
 	if e != nil {
 		return e
 	}
+
+	log.Debug("train ----------------")
+	log.Debug(trainFields.String())
+	log.Debug("predict ----------------")
+	log.Debug(predFields.String())
 
 	for _, c := range trainParsed.columns["feature_columns"] {
 		name, err := getExpressionFieldName(c)
