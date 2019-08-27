562db8b50a4bee50faace95fd31b37d12c4e99e7
terrytangyuan <terrytangyuan@gmail.com>
2019-08-27 15:44:20 -0400

    Move code to a separate script
    Signed-off-by: terrytangyuan <terrytangyuan@gmail.com>

---
+++ scripts/setup_k8s_env.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -e
+
+export MINIKUBE_WANTUPDATENOTIFICATION=false
+export MINIKUBE_WANTREPORTERRORPROMPT=false
+export MINIKUBE_HOME=$HOME
+export CHANGE_MINIKUBE_NONE_USER=true
+export KUBECONFIG=$HOME/.kube/config
+export K8S_VERSION=1.14.0
+export MINIKUBE_VERSION=1.1.1
+
+# Install kubectl and minikube (currently only used for ElasticDL integration tests with maxcompute)
+# TODO(terrytangyuan): Enable TestEnd2EndMaxComputeElasticDL test in scripts/test_maxcompute.sh once ElasticDL is open sourced
+curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v$K8S_VERSION/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
+curl -Lo minikube https://storage.googleapis.com/minikube/releases/v$MINIKUBE_VERSION/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+mkdir -p $HOME/.kube $HOME/.minikube
+touch $KUBECONFIG
+sudo minikube start --vm-driver=none --kubernetes-version=v$K8S_VERSION --cpus 2 --memory 6144
+"sudo chown -R travis: /home/travis/.minikube/"
+kubectl cluster-info
