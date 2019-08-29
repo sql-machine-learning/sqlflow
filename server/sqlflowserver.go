d282cab61998a769be1997a5962400d689604a56
雄木 <xiongmu.wy@antfin.com>
2019-08-29 18:24:01 +0800

    wip

--- server/sqlflowserver.go
+++ server/sqlflowserver.go
@@ -21,6 +21,7 @@ package server
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/golang/protobuf/proto"
@@ -55,33 +56,53 @@ func (s *Server) Run(req *pb.Request, stream pb.SQLFlow_RunServer) error {
 		}
 		defer db.Close()
 	}
-	var pr *sf.PipeReader
-	if s.enableSession == true {
-		pr = s.run(req.Sql, db, s.modelDir, req.Session)
-	} else {
-		pr = s.run(req.Sql, db, s.modelDir, nil)
-	}
 
-	defer pr.Close()
+	// FIXME(typhoonzero): split by ; can not deal with situations like
+	// "SELECT * from mytable where col <> ';';", should be fixed.
+	sqlStatements := strings.Split(req.Sql, ";")
+	fmt.Printf("@@@@@ start run: %v\n", sqlStatements)
+	for _, singleSQL := range sqlStatements {
+		if singleSQL == "" {
+			continue
+		}
+		fmt.Printf("@@@@@ start run: %s\n", singleSQL)
+		var pr *sf.PipeReader
+		startTime := time.Now().UnixNano()
+		if s.enableSession == true {
+			pr = s.run(singleSQL, db, s.modelDir, req.Session)
+		} else {
+			pr = s.run(singleSQL, db, s.modelDir, nil)
+		}
 
-	for r := range pr.ReadAll() {
-		var res *pb.Response
-		switch s := r.(type) {
-		case error:
-			return s
-		case map[string]interface{}:
-			res, err = encodeHead(s)
-		case []interface{}:
-			res, err = encodeRow(s)
-		case string:
-			res, err = encodeMessage(s)
-		default:
-			return fmt.Errorf("unrecognize run channel return type %#v", s)
+		defer pr.Close()
+
+		for r := range pr.ReadAll() {
+			var res *pb.Response
+			switch s := r.(type) {
+			case error:
+				return s
+			case map[string]interface{}:
+				res, err = encodeHead(s)
+			case []interface{}:
+				res, err = encodeRow(s)
+			case string:
+				res, err = encodeMessage(s)
+			default:
+				return fmt.Errorf("unrecognize run channel return type %#v", s)
+			}
+			if err != nil {
+				return err
+			}
+			if err := stream.Send(res); err != nil {
+				return err
+			}
 		}
-		if err != nil {
-			return err
-		}
-		if err := stream.Send(res); err != nil {
+		// Send EndOfExecution message
+		eoe := &pb.EndOfExecution{}
+		eoe.Sql = singleSQL
+		eoe.SpentTimeSeconds = time.Now().UnixNano() - startTime
+		eoeResponse := &pb.Response{Response: &pb.Response_Eoe{Eoe: eoe}}
+		if err := stream.Send(eoeResponse); err != nil {
 			return err
 		}
 	}
