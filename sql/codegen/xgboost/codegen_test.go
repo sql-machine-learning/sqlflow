592be389f2ef14c63a83915665144e41fc83c070
Yang Yang <yzhdoudou@gmail.com>
2019-09-16 15:33:20 -0700

    [Intermediate Representation] Generate the XgBoost training program

---
+++ sql/codegen/xgboost/codegen_test.go
@@ -0,0 +1,64 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xgboost
+
+import (
+	"fmt"
+	"github.com/go-sql-driver/mysql"
+	"github.com/sql-machine-learning/sqlflow/sql/codegen"
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestTrain(t *testing.T) {
+	a := assert.New(t)
+
+	cfg := &mysql.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		AllowNativePasswords: true,
+	}
+	_ = `SELECT *
+		FROM iris.train
+	TRAIN xgboost.gbtree
+	WITH
+		train.num_boost_round = 30,
+		model.objective = "multi:softprob"
+		model.eta = 3.1,
+		model.num_class = 3
+	COLUMN sepal_length, sepal_width, petal_length, petal_width
+	LABEL class
+	INTO sqlflow_models.my_xgboost_model;`
+	ir := codegen.TrainIR{
+		DataSource:       fmt.Sprintf("mysql://%s", cfg.FormatDSN()),
+		Select:           "select * from iris.train;",
+		ValidationSelect: "select * from iris.test;",
+		Estimator:        "xgboost.gbtree",
+		Attributes: []codegen.Attribute{
+			{"train.num_boost_round", 30},
+			{"model.objective", "multi:softprob"},
+			{"model.eta", 3.1},
+			{"model.num_class", 3}},
+		Features: map[string][]codegen.FeatureColumn{
+			"feature_columns": {
+				codegen.NumericColumn{&codegen.FieldMeta{"sepal_length", codegen.Float, "", []int{1}, false}},
+				codegen.NumericColumn{&codegen.FieldMeta{"sepal_width", codegen.Float, "", []int{1}, false}},
+				codegen.NumericColumn{&codegen.FieldMeta{"petal_length", codegen.Float, "", []int{1}, false}},
+				codegen.NumericColumn{&codegen.FieldMeta{"petal_width", codegen.Float, "", []int{1}, false}}}},
+		Label: codegen.NumericColumn{&codegen.FieldMeta{"class", codegen.Int, "", []int{1}, false}}}
+	_, err := Train(ir)
+	a.NoError(err)
+}
---
+++ sql/codegen/xgboost/template.go
@@ -0,0 +1,62 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package xgboost
+
+import "text/template"
+
+type trainFiller struct {
+	DataSource       string
+	TrainSelect      string
+	ValidationSelect string
+	ModelParamsJSON  string
+	TrainParamsJSON  string
+	FieldMetaJSON    string
+	LabelJSON        string
+}
+
+const trainTemplateText = `
+import xgboost as xgb
+from sqlflow_submitter.db import connect_with_data_source, db_generator
+import json
+model_params = json.loads('''{{.ModelParamsJSON}}''')
+train_params = json.loads('''{{.TrainParamsJSON}}''')
+feature_field_meta = json.loads('''{{.FieldMetaJSON}}''')
+label_field_meta = json.loads('''{{.LabelJSON}}''')
+
+# NOTE(tony): sorting is necessary to achieve consistent feature orders between training job and prediction/analysis job
+feature_column_name = sorted([k["name"] for k in feature_field_meta])
+label_name = label_field_meta["name"]
+
+feature_spec = {k['name']: k for k in feature_field_meta}
+
+conn = connect_with_data_source('''{{.DataSource}}''')
+
+def xgb_dataset(fn, dataset_sql):
+    gen = db_generator(conn.driver, conn, dataset_sql, feature_column_name, label_name, feature_spec)
+    with open(fn, 'w') as f:
+        for item in gen():
+            features, label = item
+            row_data = [str(label[0])] + ["%d:%f" % (i, v) for i, v in enumerate(features)]
+            f.write("\t".join(row_data) + "\n")
+    # TODO(yancey1989): generate group and weight text file if necessary
+    return xgb.DMatrix(fn)
+
+dtrain = xgb_dataset('train.txt', '''{{.TrainSelect}}''')
+dtest = xgb_dataset('test.txt', '''{{.ValidationSelect}}''')
+
+bst = xgb.train(model_params, dtrain, **train_params)
+bst.save_model("my_model")
+`
+
+var trainTemplate = template.Must(template.New("Train").Parse(trainTemplateText))
