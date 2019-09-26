8e52276a6005f17ccfe6895f7900bf9f79131181
Echo9573 <2663966198@qq.com>
2019-09-26 09:17:44 +0800

    A Docker image with the public dataset (#927)
    * A Docker image with the public dataset#853:Clustering Model
    
    * Add a README.md of didi folder
    
    * move top readme
    
    * update top readme
    
    * polish readme
    
    * fix database name

---
+++ doc/tutorial/didi/README.md
@@ -0,0 +1,22 @@
+# ASPARA Conference 2019
+
+## Overview
+
+This directory contains tutorials presented in the [ASPARA Conference 2019](https://www.alibabacloud.com/apsara-conference-2019). In these tutorials, we will learn how to
+
+- Train a DNN model on the [Titanic Dataset](https://www.kaggle.com/c/titanic), then use the trained model to predict on testing data.
+- Train an XGBoost model on the [Car Price Dataset](https://www.kaggle.com/c/titanic), then use the trained model to predict on the test data. Analyze the trained model using [SHAP EXPLAINER](https://github.com/slundberg/shap).
+- Train a cluster model on the [Active Power Consumption](https://www.kaggle.com/uciml/electric-power-consumption-data-set), then explore different patterns on the clustered data.
+
+## Quick Start
+
+It's quite simple to try out SQLFlow using [Docker](https://docs.docker.com/).
+
+1. Install [Docker Community Edition](https://docs.docker.com/install/).
+2. Run SQLflow by typing the command `docker run -it -p 8888:8888 sqlflow/sqlflow:didi`.
+3. Access localhost:8888 in your Web browser.
+4. Open the one of the IPython Notebook and run all cells.
+
+## Acknowledgement
+
+We thank the data scientists and software engineers from [Didi Chuxing](https://www.didiglobal.com/), especially [@Echo9573](https://github.com/Echo9573) and [@BlackPoint-CX](https://github.com/BlackPoint-CX), for contributing them.
---
+++ doc/tutorial/didi/activepower_clustering/activepower_preprocessing.py
@@ -0,0 +1,56 @@
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# coding: utf-8
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+
+import warnings
+warnings.filterwarnings('ignore')
+
+### Loding raw data
+raw = pd.read_csv('household_power_consumption.csv')
+
+### Selecting the field 'Global_active_power' for clustering
+data = raw[['Date', 'Time', 'Global_active_power']]
+data['Global_active_power'] = data['Global_active_power'].replace('?',0).astype(float)
+
+### Data reconstruction
+date = data['Date'].str.split('/', expand=True)[[0,1]].astype('str')
+dates = date[1] +'/' + date[0]
+data['Date'] = dates
+
+secs = data[data['Date']=='1/1'].Time
+days = data.Date.unique()
+
+df_gap = pd.DataFrame([], columns=secs, index = days)
+for i in days:
+    df_gap.loc[i] = data[data['Date']== i ]['Global_active_power'].T.values
+
+### Data aggregation
+df_gap_agg = pd.DataFrame([], index=df_gap.index)
+timegap = 30
+for i in range(1440//30):
+    df_gap_agg['m' + str(i+1)] = df_gap.iloc[:,(i*timegap):(i+1)*timegap].sum(1)
+
+### Data scaling
+df_gap_final = pd.DataFrame(preprocessing.MinMaxScaler().fit_transform(df_gap_agg), 
+                     columns=df_gap_agg.columns, index=df_gap_agg.index)
+df_gap_final.index.name = 'dates'
+df_gap_final = df_gap_final.reset_index()
+df_gap_final.to_csv('activepower.csv', index=False)
