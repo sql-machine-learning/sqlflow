646ce24e6dca5eefc6fb646cb8a8c3cb31b972db
Yang Yang(Tony) <29932814+tonyyang-svail@users.noreply.github.com>
2019-10-14 21:12:15 -0700

    Add attribute value range checker helper function (#1004)

---
+++ pkg/sql/codegen/attribute/checker_test.go
@@ -0,0 +1,55 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package attribute
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestFloat32RangeChecker(t *testing.T) {
+	a := assert.New(t)
+
+	checker := Float32RangeChecker(newFloat32(0.0), newFloat32(1.0), true, true)
+	a.Error(checker(float32(-1)))
+	a.NoError(checker(float32(0)))
+	a.NoError(checker(float32(0.5)))
+	a.NoError(checker(float32(1)))
+	a.Error(checker(float32(2)))
+
+	checker2 := Float32RangeChecker(newFloat32(0.0), newFloat32(1.0), false, false)
+	a.Error(checker2(float32(-1)))
+	a.Error(checker2(float32(0)))
+	a.NoError(checker2(float32(0.5)))
+	a.Error(checker2(float32(1)))
+	a.Error(checker2(float32(2)))
+}
+
+func TestIntRangeChecker(t *testing.T) {
+	a := assert.New(t)
+
+	checker := IntRangeChecker(newInt(0), newInt(2), true, true)
+	a.Error(checker(int(-1)))
+	a.NoError(checker(int(0)))
+	a.NoError(checker(int(1)))
+	a.NoError(checker(int(2)))
+	a.Error(checker(int(3)))
+
+	checker2 := IntRangeChecker(newInt(0), newInt(2), false, false)
+	a.Error(checker2(int(-1)))
+	a.Error(checker2(int(0)))
+	a.NoError(checker2(int(1)))
+	a.Error(checker2(int(2)))
+	a.Error(checker2(int(3)))
+}
