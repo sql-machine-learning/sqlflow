a6772fca9b9ab0ed923625b7a21b3d88f60e8080
Yang Yang <yzhdoudou@gmail.com>
2019-10-18 12:21:23 -0700

    Use fixed ElasticDL version in Docker image

--- scripts/build_docker_image.sh
+++ scripts/build_docker_image.sh
@@ -113,6 +113,7 @@ apt-get update && apt-get install -y docker.io sudo
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.14.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 git clone https://github.com/sql-machine-learning/elasticdl.git
 cd elasticdl
+git checkout eb93e2a48e6fe8f077c4937d8c0c5987faa9cf56
 pip install -r elasticdl/requirements.txt
 python setup.py install
 cd ..
