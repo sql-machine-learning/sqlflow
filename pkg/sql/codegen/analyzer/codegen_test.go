9c2ff6e9d58c98a835d134877f809e4a9980c78f
w7u <wg.z@qq.com>
2019-10-21 19:03:47 +0800

    add ut for codegen analyze

--- pkg/sql/codegen/analyzer/codegen_test.go
+++ pkg/sql/codegen/analyzer/codegen_test.go
@@ -12,3 +12,52 @@
 // limitations under the License.o
 
 package analyzer
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-sql-driver/mysql"
+	"github.com/stretchr/testify/assert"
+	"sqlflow.org/sqlflow/pkg/sql/codegen"
+)
+
+func TestGenAnalysis(t *testing.T) {
+	a := assert.New(t)
+	tir := mockTrainIR()
+	attrs := make(map[string]interface{})
+	attrs["shap_summary.plot_type"] = "dot"
+	attrs["others.type"] = "dot"
+	air := &codegen.AnalyzeIR{
+		DataSource: tir.DataSource,
+		Select:     "SELECT * FROM iris.train",
+		Explainer:  "TreeExplainer",
+		Attributes: attrs,
+		TrainIR:    tir,
+	}
+	_, err := GenAnalysis(air, "")
+	a.NoError(err)
+}
+
+func mockTrainIR() *codegen.TrainIR {
+	cfg := &mysql.Config{
+		User:                 "root",
+		Passwd:               "root",
+		Net:                  "tcp",
+		Addr:                 "127.0.0.1:3306",
+		AllowNativePasswords: true,
+	}
+	l := codegen.NumericColumn{FieldMeta: &codegen.FieldMeta{Name: "class"}}
+	return &codegen.TrainIR{
+		DataSource:       fmt.Sprintf("mysql://%s", cfg.FormatDSN()),
+		Select:           "select * from iris.train;",
+		ValidationSelect: "select * from iris.test;",
+		Estimator:        "xgboost.gbtree",
+		Label:            &l,
+		Attributes: map[string]interface{}{
+			"train.num_boost_round": 10,
+			"objective":             "multi:softprob",
+			"eta":                   float32(0.1),
+			"num_class":             3},
+	}
+}
