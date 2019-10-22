ec16c3058790e4d9c4094101221b448836ffcd6c
Yang Yang <yzhdoudou@gmail.com>
2019-10-22 13:03:48 -0700

    more test cases on select

--- pkg/sql/parser/split_test.go
+++ pkg/sql/parser/split_test.go
@@ -16,66 +16,89 @@ package parser
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"strings"
 	"testing"
 )
 
 func TestSplit(t *testing.T) {
 	a := assert.New(t)
 
-	//	{ // one SQL statement
-	//		sql := `select 1;`
-	//		s, err := split(sql)
-	//		a.NoError(err)
-	//		a.Equal(1, len(s))
-	//		a.Equal(`select 1;`, s[0])
-	//	}
-	//
-	//	{ // two SQL statements with comments
-	//		sql := `-- this is a comment
-	//select
-	//1; select 2;`
-	//		s, err := split(sql)
-	//		a.NoError(err)
-	//		a.Equal(2, len(s))
-	//		a.Equal(`-- this is a comment
-	//select
-	//1;`, s[0])
-	//		a.Equal(` select 2;`, s[1])
-	//	}
-	//
-	//	{ // two SQL statements, the first one is extendedSQL
-	//		sql := `select 1 to train; select 1;`
-	//		s, err := split(sql)
-	//		a.NoError(err)
-	//		a.Equal(2, len(s))
-	//		a.Equal(`select 1 to train;`, s[0])
-	//		a.Equal(` select 1;`, s[1])
-	//	}
-	//
-	//	{ // two SQL statements, the second one is extendedSQL
-	//		sql := `select 1; select 1 to train;`
-	//		s, err := split(sql)
-	//		a.NoError(err)
-	//		a.Equal(2, len(s))
-	//		a.Equal(`select 1;`, s[0])
-	//		a.Equal(` select 1 to train;`, s[1])
-	//	}
-	//
-	//	{ // three SQL statements, the second one is extendedSQL
-	//		sql := `select 1; select 1 to train; select 3;`
-	//		s, err := split(sql)
-	//		a.NoError(err)
-	//		a.Equal(3, len(s))
-	//		a.Equal(`select 1;`, s[0])
-	//		a.Equal(` select 1 to train;`, s[1])
-	//		a.Equal(` select 3;`, s[2])
-	//	}
+	selectCases := []string{
+		`select 1`,
+		`select * from my_table`,
+		`-- this is a comment
+select
+1`,
+		`SELECT
+    customerNumber,
+    checkNumber,
+    amount
+FROM
+    payments
+WHERE
+    amount = (SELECT MAX(amount) FROM payments)`}
 
-	{ // two SQL statements, the first standard SQL has an error
-		sql := `seleeeeeect 1; select 1 to train;`
+	// one standard SQL statement
+	for _, sql := range selectCases {
 		s, err := split(sql)
-		fmt.Println(err)
-		a.Error(err)
+		a.NoError(err)
+		a.Equal(1, len(s))
+		a.Equal(sql, s[0])
+	}
+
+	{ // several standard SQL statements with comments
+		sqls := strings.Join(selectCases, `;`) + `;`
+		s, err := split(sqls)
+		a.NoError(err)
+		a.Equal(len(selectCases), len(s))
+		for i := range s {
+			a.Equal(selectCases[i]+`;`, s[i])
+		}
+	}
+
+	// two SQL statements, the first one is extendedSQL
+	for _, sql := range selectCases {
+		sqls := fmt.Sprintf(`%s to train;%s;`, sql, sql)
+		s, err := split(sqls)
+		a.NoError(err)
+		a.Equal(2, len(s))
+		a.Equal(sql+` to train;`, s[0])
+		a.Equal(sql+`;`, s[1])
+	}
+
+	// two SQL statements, the second one is extendedSQL
+	for _, sql := range selectCases {
+		sqls := fmt.Sprintf(`%s;%s to train;`, sql, sql)
+		s, err := split(sqls)
+		a.NoError(err)
+		a.Equal(2, len(s))
+		a.Equal(sql+`;`, s[0])
+		a.Equal(sql+` to train;`, s[1])
+	}
+
+	// three SQL statements, the second one is extendedSQL
+	for _, sql := range selectCases {
+		sqls := fmt.Sprintf(`%s;%s to train;%s;`, sql, sql, sql)
+		s, err := split(sqls)
+		a.NoError(err)
+		a.Equal(3, len(s))
+		a.Equal(sql+`;`, s[0])
+		a.Equal(sql+` to train;`, s[1])
+		a.Equal(sql+`;`, s[2])
+	}
+
+	{ // two SQL statements, the first standard SQL has an error.
+		sql := `select select 1; select 1 to train;`
+		s, err := split(sql)
+		a.EqualError(err, `parsing "select " failed: line 1 column 7 near "" `)
+		a.Equal(0, len(s))
+	}
+
+	// two SQL statements, the second standard SQL has an error.
+	for _, sql := range selectCases {
+		sqls := fmt.Sprintf(`%s to train; select select 1;`, sql)
+		s, err := split(sqls)
+		a.EqualError(err, `parsing " select " failed: line 1 column 8 near "" `)
 		a.Equal(0, len(s))
 	}
 }
