1fc8e5d31261c16d74416922dac5689c42f83f9f
Yan Xu <yancey1989@gmail.com>
2019-10-31 16:56:50 +0800

    enhancement failed log message (#1105)
    * enhancement debug
    
    * fix ci
    
    * update log format
    
    * update

--- pkg/sql/executor.go
+++ pkg/sql/executor.go
@@ -18,6 +18,7 @@ import (
 	"database/sql"
 	"encoding/base64"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"os/exec"
@@ -458,13 +459,17 @@ func train(wr *PipeWriter, tr *extendedSelect, db *DB, cwd string, modelDir stri
 	}
 
 	cw := &logChanWriter{wr: wr}
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("\n==========Program======\n%s\n=======Program Output===========\n", program.String()))
+
+	w := io.MultiWriter(cw, &buf)
 	defer cw.Close()
 	cmd := sqlflowCmd(cwd, db.driverName)
 	cmd.Stdin = &program
-	cmd.Stdout = cw
-	cmd.Stderr = cw
+	cmd.Stdout = w
+	cmd.Stderr = w
 	if e := cmd.Run(); e != nil {
-		return fmt.Errorf("training failed %v", e)
+		return fmt.Errorf("predict failed: %v\n %s", e, buf.String())
 	}
 	m := model{workDir: cwd, TrainSelect: slct}
 	if modelDir != "" {
@@ -511,7 +516,7 @@ func pred(wr *PipeWriter, pr *extendedSelect, db *DB, cwd string, modelDir strin
 		return fmt.Errorf("loadModelMeta %v", e)
 	}
 
-	var buf bytes.Buffer
+	var program bytes.Buffer
 	if isXGBoostModel(pr.estimator) {
 		if enableIR() {
 			ir, err := generatePredictIR(pr, db.String(), cwd, modelDir)
@@ -526,9 +531,9 @@ func pred(wr *PipeWriter, pr *extendedSelect, db *DB, cwd string, modelDir strin
 			if err != nil {
 				return err
 			}
-			buf.WriteString(code)
+			program.WriteString(code)
 		} else {
-			if e := genXGBoost(&buf, pr, nil, fts, db, session); e != nil {
+			if e := genXGBoost(&program, pr, nil, fts, db, session); e != nil {
 				return fmt.Errorf("genXGBoost %v", e)
 			}
 		}
@@ -546,22 +551,29 @@ func pred(wr *PipeWriter, pr *extendedSelect, db *DB, cwd string, modelDir strin
 			if err != nil {
 				return err
 			}
-			buf.WriteString(code)
+			program.WriteString(code)
 		} else {
-			if e := genTF(&buf, pr, nil, fts, db, session); e != nil {
+			if e := genTF(&program, pr, nil, fts, db, session); e != nil {
 				return fmt.Errorf("genTF %v", e)
 			}
 		}
 	}
 
+	var buf bytes.Buffer
+	buf.WriteString(fmt.Sprintf("\n==========Program======\n%s\n=======Program Output===========\n", program.String()))
+
 	cw := &logChanWriter{wr: wr}
+	w := io.MultiWriter(cw, &buf)
 	defer cw.Close()
 	cmd := sqlflowCmd(cwd, db.driverName)
 	cmd.Env = append(os.Environ())
-	cmd.Stdin = &buf
-	cmd.Stdout = cw
-	cmd.Stderr = cw
-	return cmd.Run()
+	cmd.Stdin = &program
+	cmd.Stdout = w
+	cmd.Stderr = w
+	if e := cmd.Run(); e != nil {
+		return fmt.Errorf("predict failed: %v\n %s", e, buf.String())
+	}
+	return nil
 }
 
 func analyze(wr *PipeWriter, pr *extendedSelect, db *DB, cwd, modelDir string) error {
