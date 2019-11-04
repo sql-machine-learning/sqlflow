9e6ce3d73f68336f30439f8b3f94b899cfdbce33
雄木 <xiongmu.wy@antfin.com>
2019-11-04 16:15:02 +0800

    fix typo

--- pkg/sql/create_train_val_test.go
+++ pkg/sql/create_train_val_test.go
@@ -27,7 +27,7 @@ WHERE a.class=b.class
 LIMIT 7
 `
 	// TODO(typhoonzero): original complex statement will error when run on Hive
-	// (CI resource not enouph, see: https://community.cloudera.com/t5/Support-Questions/HIVE-return-code-2-from-org-apache-hadoop-hive-ql-exec-mr/td-p/19750).
+	// (CI resource not enough, see: https://community.cloudera.com/t5/Support-Questions/HIVE-return-code-2-from-org-apache-hadoop-hive-ql-exec-mr/td-p/19750).
 	// If we are moving to use a separate SQL statement for retrieving this test can be removed.
 	testTrainAndValDataset = `
 SELECT * from iris.train`
