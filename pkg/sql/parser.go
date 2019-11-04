78fcf4e509e7815dcb58166298d77571f58ea8d5
shendiaomo <shendiaomo@gmail.com>
2019-11-04 20:42:46 +0800

    Correct more misspells and typos

--- pkg/sql/parser.go
+++ pkg/sql/parser.go
@@ -101,7 +101,7 @@ type trainClause struct {
 
 /* If no FOR in the COLUMN, the key is "" */
 type columnClause map[string]exprlist
-type filedClause exprlist
+type fieldClause exprlist
 
 type attrs map[string]*expr
 
