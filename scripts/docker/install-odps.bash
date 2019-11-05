10a57654036698d13076a22dd7672a068664b3b9
Yi Wang <yi.wang.2005@gmail.com>
2019-11-05 15:32:32 -0800

    Add more install scripts

---
+++ scripts/docker/install-odps.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Install odpscmd for submitting Alps predict job with ODPS UDF script.
+# TODO(Yancey1989): using gomaxcompute instead of the odpscmd command-line tool.
+wget -q http://docs-aliyun.cn-hangzhou.oss.aliyun-inc.com/assets/attach/119096/cn_zh/1557995455961/odpscmd_public.zip
+unzip -qq odpscmd_public.zip -d /usr/local/odpscmd
+ln -s /usr/local/odpscmd/bin/odpscmd /usr/local/bin/odpscmd
+rm -rf odpscmd_public.zip
