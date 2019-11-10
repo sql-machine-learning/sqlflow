ec08d06dde73ca46733a54a35f93a7a147883389
Yang Yang(Tony) <29932814+tonyyang-svail@users.noreply.github.com>
2019-11-09 20:34:33 -0800

    Enable Java tests in CI (#1163)
    * Enable Java test in CI
    
    * make downloading quiet

--- scripts/test_java.sh
+++ scripts/test_java.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # Copyright 2019 The SQLFlow Authors. All rights reserved.
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +12,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+
 set -e
 
-apt-get update && apt-get install -y openjdk-8-jdk maven
+cd java/parser
+
+# Make downloading quiet.
+# Downloading logs is about 6k lines, which makes viewing TravisCI log difficult
+export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+
+# -B means batch mode, looks like batch mode is required to make downloading quiet
+mvn test -B
 
