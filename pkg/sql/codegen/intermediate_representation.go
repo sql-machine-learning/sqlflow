36b34f5b006743747b955bf06f62f858ddb107ca
雄木 <xiongmu.wy@antfin.com>
2019-11-21 22:21:34 +0800

    wip

--- pkg/sql/codegen/intermediate_representation.go
+++ pkg/sql/codegen/intermediate_representation.go
@@ -11,7 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:generate protoc -I proto sqlflow.proto --go_out=plugins=grpc:proto
+//go:generate protoc -I proto intermediate_representation.proto --go_out=proto
 
 package codegen
 
