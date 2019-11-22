986c3f636ffeef21df362aad3793c58e41cb6115
雄木 <xiongmu.wy@antfin.com>
2019-11-22 16:36:29 +0800

    separate elasticdl tests

--- scripts/test_elasticdl.sh
+++ scripts/test_elasticdl.sh
@@ -21,15 +21,17 @@ if [ "$MAXCOMPUTE_AK" = "" ] || [ "$MAXCOMPUTE_SK" == "" ]; then
   echo "skip maxcompute test because the env MAXCOMPUTE_AK or MAXCOMPUTE_SK is empty"
   exit 0
 fi
-# NOTE: we have already installed sqlflow_submitter under python installation path
-# using latest develop branch, but when testing on CI, we need to use the code in
-# the current pull request.
-export PYTHONPATH=$GOPATH/src/sqlflow.org/sqlflow/python
 
-go generate ./...
-go install ./...
+# End-to-end test for ElasticDL
+export SQLFLOW_submitter=elasticdl
+cd /elasticdl
+# Build base images for ElasticDL jobs
+docker build -t elasticdl:dev -f elasticdl/docker/Dockerfile.dev .
+docker build -t elasticdl:ci -f elasticdl/docker/Dockerfile.ci .
+# Set up necessary RBAC roles for k8s cluster
+kubectl apply -f elasticdl/manifests/examples/elasticdl-rbac.yaml
+cd -
+SQLFLOW_log_level=debug go test -p 1 -v ./cmd/... -run TestEnd2EndMaxComputeElasticDL
 
-# -p 1 is necessary since tests in different packages are sharing the same database
-# ref: https://stackoverflow.com/a/23840896
-# TODO(Yancey1989): enable all the unit test for the maxcompute
-SQLFLOW_log_level=debug go test -p 1 -v ./cmd/... -run TestEnd2EndMaxCompute
+cd /elasticdl
+bash scripts/validate_job_status.sh odps
