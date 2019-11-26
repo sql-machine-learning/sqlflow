bea24c3e6d3891a272e0e97c51ef867c1c33c6e6
shendiaomo <shendiaomo@gmail.com>
2019-11-26 16:18:38 +0800

    Fix inconsistent indentaion and redundant raw string prefix

--- python/extract_docstring.py
+++ python/extract_docstring.py
@@ -21,51 +21,51 @@ import sqlflow_models
 from tensorflow.estimator import *
 
 def parse_ctor_args(f, prefix=''):
-  r"""Given an class or function, parse the class constructor/function details
-  from its docstring
-
-  For example, the docstring of sqlflow_models.DNNClassifier.__init__ is:
-  '''DNNClassifier
-  :param feature_columns: feature columns.
-  :type feature_columns: list[tf.feature_column].
-  :param hidden_units: number of hidden units.
-  :type hidden_units: list[int].
-  :param n_classes: List of hidden units per layer.
-  :type n_classes: int.
-  '''
-  Calling parse_ctor_args(sqlflow_models.DNNClassifier, ":param") returns:
-  {
-      "feature_columns": "feature columns. :type feature_columns: list[tf.feature_column].",
-      "hidden_units": "number of hidden units. :type hidden_units: list[int].",
-      "n_classes": "List of hidden units per layer. :type n_classes: int."
-  }
-  And calling parse_ctor_args(parse_ctor_args) returns:
-  {
-      "f": "The class or function whose docstring to parse",
-      "prefix": "The prefix of parameters in the docstring"
-  }
-
-  Args:
-    f: The class or function whose docstring to parse
-    prefix: The prefix of parameters in the docstring
-
-  Returns:
-    A dict with parameters as keys and splitted docstring as values
-  """
-
-  try:
-      doc = f.__init__.__doc__
-  except:
-      doc = ''
-  doc = doc if doc else f.__doc__
-  arg_list = list(inspect.signature(f).parameters)
-  args = '|'.join(arg_list)
-  arg_re = re.compile(r'(?<=\n)\s*%s\s*(%s)\s*:\s*' % (prefix, args), re.MULTILINE)
-  total = arg_re.split(six.ensure_str(doc))
-  # Trim *args and **kwargs if any:
-  total[-1] = re.sub(r'(?<=\n)\s*[\\*]+kwargs\s*:.*', '', total[-1], 1, re.M|re.S)
+    """Given an class or function, parse the class constructor/function details
+    from its docstring
   
-  return dict(zip(total[1::2], [' '.join(doc.split()).replace("`", "'") for doc in total[2::2]]))
+    For example, the docstring of sqlflow_models.DNNClassifier.__init__ is:
+    '''DNNClassifier
+    :param feature_columns: feature columns.
+    :type feature_columns: list[tf.feature_column].
+    :param hidden_units: number of hidden units.
+    :type hidden_units: list[int].
+    :param n_classes: List of hidden units per layer.
+    :type n_classes: int.
+    '''
+    Calling parse_ctor_args(sqlflow_models.DNNClassifier, ":param") returns:
+    {
+        "feature_columns": "feature columns. :type feature_columns: list[tf.feature_column].",
+        "hidden_units": "number of hidden units. :type hidden_units: list[int].",
+        "n_classes": "List of hidden units per layer. :type n_classes: int."
+    }
+    And calling parse_ctor_args(parse_ctor_args) returns:
+    {
+        "f": "The class or function whose docstring to parse",
+        "prefix": "The prefix of parameters in the docstring"
+    }
+  
+    Args:
+      f: The class or function whose docstring to parse
+      prefix: The prefix of parameters in the docstring
+  
+    Returns:
+      A dict with parameters as keys and splitted docstring as values
+    """
+  
+    try:
+        doc = f.__init__.__doc__
+    except:
+        doc = ''
+    doc = doc if doc else f.__doc__
+    arg_list = list(inspect.signature(f).parameters)
+    args = '|'.join(arg_list)
+    arg_re = re.compile(r'(?<=\n)\s*%s\s*(%s)\s*:\s*' % (prefix, args), re.MULTILINE)
+    total = arg_re.split(six.ensure_str(doc))
+    # Trim *args and **kwargs if any:
+    total[-1] = re.sub(r'(?<=\n)\s*[\\*]+kwargs\s*:.*', '', total[-1], 1, re.M|re.S)
+    
+    return dict(zip(total[1::2], [' '.join(doc.split()).replace("`", "'") for doc in total[2::2]]))
 
 if __name__ == "__main__":
     param_doc = {}  # { "class_names": {"parameters": "splitted docstrings"} }
