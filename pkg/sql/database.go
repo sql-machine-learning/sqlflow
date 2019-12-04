1d810834676a04a4d72de50fbfd41332aa05c8ba
Yan Xu <yancey1989@gmail.com>
2019-12-04 17:35:28 +0800

    do not enumeration the driver name (#1329)

--- pkg/sql/database.go
+++ pkg/sql/database.go
@@ -45,14 +45,22 @@ func open(datasource string) (*DB, error) {
 	}
 	db := &DB{driverName: dses[0], dataSourceName: dses[1]}
 
-	var err error
-	switch db.driverName {
-	case "mysql", "hive", "maxcompute":
-		db.DB, err = sql.Open(db.driverName, db.dataSourceName)
-	default:
-		return nil, fmt.Errorf("sqlflow currently doesn't support DB %v", db.driverName)
-	}
+	err := openDB(db)
 	return db, err
+}
+
+func openDB(db *DB) error {
+	var err error
+	for _, d := range sql.Drivers() {
+		if db.driverName == d {
+			db.DB, err = sql.Open(db.driverName, db.dataSourceName)
+			if err != nil {
+				return err
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf("sqlflow currently doesn't support DB %s", db.driverName)
 }
 
 // NewDB returns a DB object with verifying the datasource name.
