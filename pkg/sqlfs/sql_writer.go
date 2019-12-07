6f21fa499de76e9118d23aa1d640bce728d1d68f
Yi Wang <yi.wang.2005@gmail.com>
2019-12-06 23:00:10 -0800

    Add sql_writer.go and test

---
+++ pkg/sqlfs/sql_writer.go
@@ -0,0 +1,58 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlfs
+
+import (
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+	"io"
+)
+
+func flushToSQLTable(db *sql.DB, table string) func([]byte) error {
+	row := 0
+	return func(buf []byte) error {
+		if db == nil {
+			return fmt.Errorf("flushToSQLTable: no database connection")
+		}
+
+		if len(buf) > 0 {
+			block := base64.StdEncoding.EncodeToString(buf)
+			query := fmt.Sprintf("INSERT INTO %s (id, block) VALUES(%d, '%s')",
+				table, row, block)
+			if _, e := db.Exec(query); e != nil {
+				return fmt.Errorf("cannot flush to table %s: %v", table, e)
+			}
+			buf = buf[:0]
+			row++
+		}
+		return nil
+	}
+}
+
+func noopWrapUp() error {
+	return nil
+}
+
+func newSQLWriter(db *sql.DB, driver, table string) (io.WriteCloser, error) {
+	if e := dropTable(db, table); e != nil {
+		return nil, fmt.Errorf("cannot drop table %s: %v", table, e)
+	}
+	if e := createTable(db, driver, table); e != nil {
+		return nil, fmt.Errorf("cannot create table %s: %v", table, e)
+	}
+
+	const bufSize = 32 * 1024
+	return newFlushWriteCloser(flushToSQLTable(db, table), noopWrapUp, bufSize), nil
+}
---
+++ pkg/sqlfs/sql_writer_test.go
@@ -0,0 +1,94 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package sqlfs
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewSQLWriter(t *testing.T) {
+	a := assert.New(t)
+
+	fn := fmt.Sprintf("%s.unitest%d", testDatabaseName, rand.Int())
+	w, e := newSQLWriter(testDB, testDriver, fn)
+	a.NoError(e)
+	a.NotNil(w)
+	defer w.Close()
+
+	has, e1 := hasTable(testDB, fn)
+	a.NoError(e1)
+	a.True(has)
+
+	a.NoError(dropTable(testDB, fn))
+}
+
+func TestSQLWriterWriteAndRead(t *testing.T) {
+	testDriver = getEnv("SQLFLOW_TEST_DB", "mysql")
+	a := assert.New(t)
+
+	fn := fmt.Sprintf("%s.unitest%d", testDatabaseName, rand.Int())
+	w, e := newSQLWriter(testDB, testDriver, fn)
+	a.NoError(e)
+	a.NotNil(w)
+
+	// A small output.
+	buf := []byte("\n\n\n")
+	n, e := w.Write(buf)
+	a.NoError(e)
+	a.Equal(len(buf), n)
+
+	// A big output.
+	buf = make([]byte, bufSize+1)
+	for i := range buf {
+		buf[i] = 'x'
+	}
+	n, e = w.Write(buf)
+	a.NoError(e)
+	a.Equal(len(buf), n)
+
+	a.NoError(w.Close())
+
+	r, e := Open(testDB, fn)
+	a.NoError(e)
+	a.NotNil(r)
+
+	// A small read
+	buf = make([]byte, 2)
+	n, e = r.Read(buf)
+	a.NoError(e)
+	a.Equal(2, n)
+	a.Equal(2, strings.Count(string(buf), "\n"))
+
+	// A big read of rest
+	buf = make([]byte, bufSize*2)
+	n, e = r.Read(buf)
+	a.Equal(io.EOF, e)
+	a.Equal(bufSize+2, n)
+	a.Equal(1, strings.Count(string(buf), "\n"))
+	a.Equal(bufSize+1, strings.Count(string(buf), "x"))
+
+	// Another big read
+	n, e = r.Read(buf)
+	a.Equal(io.EOF, e)
+	a.Equal(0, n)
+	a.NoError(r.Close())
+
+	a.NoError(dropTable(testDB, fn))
+}
