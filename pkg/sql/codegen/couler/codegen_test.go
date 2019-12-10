9c78c0cf874b50a599d4f80840afc325f882a195
Yancey1989 <yancey1989@gmail.com>
2019-12-10 17:52:27 +0800

    add couler cluster config

--- pkg/sql/codegen/couler/codegen_test.go
+++ pkg/sql/codegen/couler/codegen_test.go
@@ -15,6 +15,8 @@ package couler
 
 import (
 	"fmt"
+	"io/ioutil"
+	"os"
 	"regexp"
 	"testing"
 
@@ -74,4 +76,83 @@ INTO sqlflow_models.my_xgboost_model;
 					&ir.NumericColumn{&ir.FieldMeta{"petal_width", ir.Float, "", []int{1}, false, nil, 0}}}},
 			Label: &ir.NumericColumn{&ir.FieldMeta{"class", ir.Int, "", []int{1}, false, nil, 0}}},
 	}
+}
+
+var testCoulerClusterConfig = `
+class K8s(object):
+    def __init__(self):
+        pass
+
+    def with_pod(self, template):
+        self._with_medadata(template)
+        self._with_tolerations(template)
+        return template
+
+    def _with_medadata(self, template):
+        template["metadata"] = {}
+        template["metadata"]["namespace"] = "sqlflow"
+    
+    def _with_tolerations(self, template):
+        template["tolerations"] = list()
+        template["tolerations"].append({
+          "effect": "NoSchedule",
+          "key": "key",
+          "value": "value",
+          "operator": "Equal"
+        })
+
+cluster = K8s()
+`
+
+var testArgoYAML = `
+apiVersion: argoproj.io/v1alpha1
+kind: Workflow
+metadata:
+  generateName: workflow-
+  namespace: sqlflow
+spec:
+  entrypoint: workflow
+  templates:
+    - name: workflow
+      steps:
+        - - name: workflow-2-2
+            template: workflow-2
+    - name: workflow-2
+      container:
+        image: docker/whalesay
+        command:
+          - bash
+          - -c
+          - 'echo "SQLFlow bridges AI and SQL engine."'
+  		tolerations:
+			- effect: NoSchedule
+				key: key
+				value: value
+				operator: Equal
+`
+var testCoulerProgram = `
+import couler.argo as couler
+couler.run_container(image="docker/whalesay", command='echo "SQLFlow bridges AI and SQL engine."')
+`
+
+func TestWriteArgoYamlWithClusterConfig(t *testing.T) {
+	a := assert.New(t)
+	tmpFile, e := ioutil.TempFile("/tmp", "sqlflow-couler*.py")
+	a.NoError(e)
+	_, e = tmpFile.Write([]byte(testCoulerProgram))
+	a.NoError(e)
+
+	tmpFile2, e := ioutil.TempFile("/tmp", "sqlflow-cluster*.py")
+	a.NoError(e)
+	_, e = tmpFile2.Write([]byte(testCoulerClusterConfig))
+	a.NoError(e)
+
+	os.Setenv("SQLFLOW_COULER_CLUSTER_CONFIG", tmpFile2.Name())
+	argoFile, e := writeArgoFile(tmpFile.Name())
+	a.NoError(e)
+
+	out, e := ioutil.ReadFile(argoFile)
+	a.NoError(e)
+
+	a.Equal(out, testArgoYAML)
 }
