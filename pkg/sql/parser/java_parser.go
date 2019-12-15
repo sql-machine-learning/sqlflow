cd611dbe92ddc432f3f1ca6ab757f955f1bb8cd9
Yi Wang <yi.wang.2005@gmail.com>
2019-12-14 18:42:56 -0800

    Separate parser parser.go (#1391)
    * Rename package sql/tpp into sql/parser
    
    * Separate tidb_parser.go from parser.go
    
    * Add type javaParser
    
    * Rename parser.go into java_parser.go
    
    * Rename tpp.go into third_party_parser.go
    
    * Add unit tests
    
    * Connect DBMS Hive with language HiveQL
    
    * Update

---
+++ pkg/sql/parser/java_parser.go
@@ -0,0 +1,89 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package parser
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path/filepath"
+)
+
+type javaParser struct {
+	// TODO(yi): As we are going to replace the command-line
+	// parser adaptor to gRPC servers, we will need to add gRPC
+	// clients here.
+	typ string
+}
+
+// typ should be either "hiveql" or "calcite".
+func newJavaParser(typ string) *javaParser {
+	return &javaParser{typ: typ}
+}
+
+type parseResult struct {
+	Statements []string `json:"statements"`
+	Position   int      `json:"position"`
+	Error      string   `json:"error"`
+}
+
+func (p *javaParser) Type() string {
+	return "java"
+}
+
+func (p *javaParser) Parse(program string) ([]string, int, error) {
+	// cwd is used to store train scripts and save output models.
+	cwd, err := ioutil.TempDir("/tmp", "sqlflow")
+	if err != nil {
+		return nil, -1, err
+	}
+	defer os.RemoveAll(cwd)
+
+	inputFile := filepath.Join(cwd, "input.sql")
+	outputFile := filepath.Join(cwd, "output.json")
+	if err := ioutil.WriteFile(inputFile, []byte(program), 0755); err != nil {
+		return nil, -1, err
+	}
+
+	// TODO(yi): It is very expensive to start a Java process.  It
+	// slows down SQLFlow server's QPS if for every parsing
+	// operation, we'd have to start a Java process.
+	cmd := exec.Command("java",
+		"-cp", "/opt/sqlflow/parser/parser-1.0-SNAPSHOT-jar-with-dependencies.jar",
+		"org.sqlflow.parser.ParserAdaptorCmd",
+		"-p", p.typ,
+		"-i", inputFile,
+		"-o", outputFile)
+	if output, err := cmd.CombinedOutput(); err != nil {
+		return nil, -1, fmt.Errorf("%s %v", output, err)
+	}
+
+	output, err := ioutil.ReadFile(outputFile)
+	if err != nil {
+		return nil, -1, err
+	}
+
+	var pr parseResult
+	if err = json.Unmarshal(output, &pr); err != nil {
+		return nil, -1, err
+	}
+
+	if pr.Error != "" {
+		return nil, -1, fmt.Errorf(pr.Error)
+	}
+
+	return pr.Statements, pr.Position, nil
+}
---
+++ pkg/sql/parser/third_party_parser.go
@@ -0,0 +1,34 @@
+// Copyright 2019 The SQLFlow Authors. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package parser
+
+// ThirdPartyParser abstract a parser of a SQL engine, for example,
+// Hive, MySQL, TiDB, MaxCompute.
+type ThirdPartyParser interface {
+	Parse(program string) ([]string, int, error)
+	Type() string
+}
+
+// NewThirdPartyParser instantiates a parser.
+func NewThirdPartyParser(typ string) ThirdPartyParser {
+	switch typ {
+	case "mysql", "tidb":
+		return newTiDBParser()
+	case "hive", "hiveql":
+		return newJavaParser("hiveql")
+	case "calcite", "maxcompute":
+		return newJavaParser("calcite")
+	}
+	return nil
+}
