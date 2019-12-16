df76943dc40ba96ad7245c3680b99c555983e876
Yi Wang <yi.wang.2005@gmail.com>
2019-12-16 11:51:21 -0800

    Move pkg/sql/lexer*.go to pkg/parser

--- pkg/parser/lexer.go
+++ pkg/parser/lexer.go
@@ -11,9 +11,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package sql
+package parser
 
 import (
+	"log"
 	"regexp"
 	"strings"
 	"unicode"
