2d51e127e12164eeca009064c89959d9ef55b333
雄木 <xiongmu.wy@antfin.com>
2019-12-19 20:14:02 +0800

    add check supported metrics

--- python/sqlflow_submitter/tensorflow/metrics.py
+++ python/sqlflow_submitter/tensorflow/metrics.py
@@ -13,39 +13,18 @@
 
 import tensorflow as tf
 
-def tf_classification_metrics(labels, predictions):
-    print(predictions)
-    accuracy = tf.keras.metrics.Accuracy(name="accuracy")
-    accuracy.update_state(y_true=labels, y_pred=predictions['class_ids'])
-    
-    auc = tf.keras.metrics.AUC(name="auc", num_thresholds=2000)
-    auc.update_state(y_true=labels, y_pred=predictions['probabilities'])
-
-    # TODO(typhoonzero): precision & recall is defined for a specific class
-    # since we may encounter multi-class classification here, we do not calculate
-    # precision by default. We need find a way to determine whether
-    # the job is binary classificaion.
-    return {"accuracy": accuracy, "auc": auc}
-
-
-def tf_regression_metrics(labels, predictions):
-    rmse = tf.keras.metrics.RootMeanSquaredError(name="rmse")
-    rmse.update_state(y_true=labels, y_pred=predictions['logits'])
-
-    mae = tf.keras.metrics.MeanAbsoluteError(name="mae")
-    mae.update_state(y_true=labels, y_pred=predictions['logits'])
-
-    mape = tf.keras.metrics.MeanAbsolutePercentageError(name="mape")
-    mape.update_state(y_true=labels, y_pred=predictions['logits'])
-
-    return {"rmse": rmse, "mae": mae, "mape": mape}
-
 metric_names_use_class_id = ["Accuracy", "Precision", "Recall", "TruePositives", "TrueNegatives", "FalsePositives", "FalseNegatives"]
 metric_names_use_probabilities = ["BinaryAccuracy", "CategoricalAccuracy", "TopKCategoricalAccuracy", "AUC"]
 metric_names_use_logits = ["MeanAbsoluteError", "MeanAbsolutePercentageError", "MeanSquaredError", "RootMeanSquaredError"]
 supported_metrics = metric_names_use_class_id + metric_names_use_probabilities + metric_names_use_logits
 
+def check_supported(metrics):
+    for mn in metrics:
+        if mn not in supported_metrics:
+            raise ValueError("metric name not supported %s, supported metrics: %s" % (mn, supported_metrics))
+
 def get_tf_metrics(metrics):
+    check_supported(metrics)
     def tf_metrics_func(labels, predictions):
         metric_dict = {}
         for mn in metrics:
@@ -61,6 +40,7 @@ def get_tf_metrics(metrics):
     return tf_metrics_func
 
 def get_keras_metrics(metrics):
+    check_supported(metrics)
     m = []
     for mn in metrics:
         m.append(eval("tf.keras.metrics.%s()" % mn))
