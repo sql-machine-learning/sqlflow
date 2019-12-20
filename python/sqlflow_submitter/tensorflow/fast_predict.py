859eb5927a5122018e9b4c163c92b938f7f4a827
雄木 <xiongmu.wy@antfin.com>
2019-12-20 16:02:34 +0800

    WIP: polish tensorflow submitter

---
+++ python/sqlflow_submitter/tensorflow/fast_predict.py
@@ -0,0 +1,46 @@
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+class FastPredict:
+    def __init__(self, estimator, input_fn):
+        self.estimator = estimator
+        self.first_run = True
+        self.closed = False
+        self.input_fn = input_fn
+
+    def _create_generator(self):
+        while not self.closed:
+            yield self.next_features[0], self.next_features[1]
+
+    def predict(self, feature_batch):
+        self.next_features = feature_batch
+        if self.first_run:
+            self.batch_size = len(feature_batch)
+            self.predictions = self.estimator.predict(
+                input_fn=self.input_fn(self._create_generator))
+            self.first_run = False
+        elif self.batch_size != len(feature_batch):
+            raise ValueError("All batches must be of the same size. First-batch:" + str(self.batch_size) + " This-batch:" + str(len(feature_batch)))
+
+        results = []
+        for _ in range(self.batch_size):
+            results.append(next(self.predictions))
+        return results
+
+    def close(self):
+        self.closed = True
+        try:
+            next(self.predictions)
+        except Exception as e:
+            print("Exception in fast_predict. This is probably OK: %s" % e)
---
+++ python/sqlflow_submitter/tensorflow/hooks.py
@@ -0,0 +1,45 @@
+# Copyright 2019 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+
+# NOTE(typhoonzero): tf.estimator.LoggingTensorHook is only availble using
+# Tensorflow version > 2.0.
+
+class PrintStatusHook(tf.estimator.LoggingTensorHook):
+    def __init__(self, prefix="", every_n_iter=None, every_n_secs=None,
+            at_end=False, formatter=None):
+        super().__init__([], every_n_iter=every_n_iter, every_n_secs=every_n_secs,
+            at_end=at_end, formatter=formatter)
+        self.prefix = prefix
+
+    def before_run(self, run_context):
+        self._should_trigger = self._timer.should_trigger_for_step(self._iter_count)
+        loss_vars = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.LOSSES)
+        step_vars = tf.compat.v1.get_collection(tf.compat.v1.GraphKeys.GLOBAL_STEP)
+        fetch = {"loss": loss_vars[0], "step": step_vars[0]}
+        if self._should_trigger:
+            return tf.estimator.SessionRunArgs(fetch)
+        else:
+            return None
+
+    def _log_tensors(self, tensor_values):
+        elapsed_secs, _ = self._timer.update_last_triggered_step(self._iter_count)
+        stats = []
+        for k in tensor_values.keys():
+            stats.append("%s = %s" % (k, tensor_values[k]))
+        if self.prefix == "eval":
+            print("============Evaluation=============")
+        print("%s: %s" % (self.prefix, ", ".join(stats)))
+        if self.prefix == "eval":
+            print("============Evaluation End=============")
