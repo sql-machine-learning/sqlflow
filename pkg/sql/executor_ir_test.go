6c39675ec881ff54fc9946252d0d3174128f6168
雄木 <xiongmu.wy@antfin.com>
2019-12-26 11:28:51 +0800

    update

--- pkg/sql/executor_ir_test.go
+++ pkg/sql/executor_ir_test.go
@@ -128,7 +128,7 @@ WITH
 LABEL class
 INTO sqlflow_models.my_xgboost_model_by_program;
 
-SELECT sepal_length as sl, sepal_width as sw, class FROM iris.test
+SELECT sepal_length as sl, sepal_width as sw FROM iris.test
 TO PREDICT iris.predict.class
 USING sqlflow_models.my_xgboost_model_by_program;
 
@@ -138,7 +138,6 @@ USING TreeExplainer;
 `, modelDir, database.GetSessionFromTestingDB())
 		a.True(goodStream(stream.ReadAll()))
 	})
-
 }
 
 func TestExecuteXGBoostClassifier(t *testing.T) {
