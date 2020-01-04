35f5f89197e78f57d27114524f8073791f82b6d6
Wu Yi <typhoonzero1986@gmail.com>
2020-01-04 10:30:54 +0800

    Add explain into grammar (#1613)
    * add explain into grammar
    
    * update

--- pkg/sql/ir_generator.go
+++ pkg/sql/ir_generator.go
@@ -304,6 +304,7 @@ func generateExplainStmt(slct *parser.SQLFlowSelectStmt, connStr, modelDir strin
 		Attributes: attrs,
 		Explainer:  slct.Explainer,
 		TrainStmt:  trainStmt,
+		Into:       slct.ExplainInto,
 	}
 
 	if getTrainStmtFromModel {
