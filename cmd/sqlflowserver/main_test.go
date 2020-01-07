239a87659894c00635e7fe5f3e5bf57409801fdf
shendiaomo <shendiaomo@gmail.com>
2020-01-07 23:51:12 +0800

    Make WITH clause in TRAIN statement optional (#1637)
    * Make WITH clause in TRAIN statement optional
    
    * Fix grammar error

--- cmd/sqlflowserver/main_test.go
+++ cmd/sqlflowserver/main_test.go
@@ -707,6 +707,16 @@ func CaseTrainBoostedTreesEstimatorAndExplain(t *testing.T) {
 	}
 }
 
+func CaseTrainLinearClassifier(t *testing.T) {
+	a := assert.New(t)
+	trainSQL := fmt.Sprintf(`SELECT * FROM iris.train WHERE class !=2
+TO TRAIN LinearClassifier LABEL class INTO %s;`, caseInto)
+	_, _, err := connectAndRunSQL(trainSQL)
+	if err != nil {
+		a.Fail("Run trainSQL error: %v", err)
+	}
+}
+
 func CaseTrainSQLWithMetrics(t *testing.T) {
 	a := assert.New(t)
 	trainSQL := `SELECT * FROM iris.train WHERE class!=2
