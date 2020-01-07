0a9971d864e31ff1b9d724e387e3af8d4b273f40
Yang Yang <yzhdoudou@gmail.com>
2020-01-07 17:18:19 +0800

    Remove LegacyParse in verifier_test.go

--- pkg/verifier/verifier_test.go
+++ pkg/verifier/verifier_test.go
@@ -14,6 +14,7 @@
 package verifier
 
 import (
+	"fmt"
 	"os"
 	"testing"
 
@@ -22,40 +23,53 @@ import (
 	"sqlflow.org/sqlflow/pkg/parser"
 )
 
+func parseOneStatement(statement string) (*parser.SQLFlowSelectStmt, error) {
+	sqls, err := parser.Parse("mysql", statement)
+	if err != nil {
+		return nil, err
+	}
+	if len(sqls) != 1 {
+		return nil, fmt.Errorf(`len(sqls) != 1, actual %d`, len(sqls))
+	}
+
+	return sqls[0].SQLFlowSelectStmt, nil
+}
+
 func TestVerify(t *testing.T) {
 	if os.Getenv("SQLFLOW_TEST_DB") == "hive" {
 		t.Skip("in Hive, db_name.table_name.field_name will raise error, because . operator is only supported on struct or list of struct types")
 	}
 
 	a := assert.New(t)
-	r, e := parser.LegacyParse(`SELECT * FROM churn.train LIMIT 10;`)
-	a.NoError(e)
-	fts, e := Verify(r.StandardSelect.String(), database.GetTestingDBSingleton())
-	a.NoError(e)
-	a.Equal(21, len(fts))
 
-	r, e = parser.LegacyParse(`SELECT Churn, churn.train.Partner,TotalCharges FROM churn.train LIMIT 10;`)
-	a.NoError(e)
-	fts, e = Verify(r.StandardSelect.String(), database.GetTestingDBSingleton())
-	a.NoError(e)
-	a.Equal(3, len(fts))
+	{
+		fts, e := Verify(`SELECT * FROM churn.train LIMIT 10;`, database.GetTestingDBSingleton())
+		a.NoError(e)
+		a.Equal(21, len(fts))
+	}
 
-	typ, ok := fts.Get("churn")
-	a.True(ok)
-	a.Contains([]string{"VARCHAR(255)", "VARCHAR"}, typ)
+	{
+		fts, e := Verify(`SELECT Churn, churn.train.Partner,TotalCharges FROM churn.train LIMIT 10;`, database.GetTestingDBSingleton())
+		a.NoError(e)
+		a.Equal(3, len(fts))
 
-	typ, ok = fts.Get("partner")
-	a.True(ok)
-	a.Contains([]string{"VARCHAR(255)", "VARCHAR"}, typ)
+		typ, ok := fts.Get("churn")
+		a.True(ok)
+		a.Contains([]string{"VARCHAR(255)", "VARCHAR"}, typ)
 
-	typ, ok = fts.Get("totalcharges")
-	a.True(ok)
-	a.Equal("FLOAT", typ)
+		typ, ok = fts.Get("partner")
+		a.True(ok)
+		a.Contains([]string{"VARCHAR(255)", "VARCHAR"}, typ)
+
+		typ, ok = fts.Get("totalcharges")
+		a.True(ok)
+		a.Equal("FLOAT", typ)
+	}
 }
 
 func TestVerifyColumnNameAndType(t *testing.T) {
 	a := assert.New(t)
-	trainParse, e := parser.LegacyParse(`SELECT gender, tenure, TotalCharges
+	trainParse, e := parseOneStatement(`SELECT gender, tenure, TotalCharges
 FROM churn.train LIMIT 10
 TO TRAIN DNNClassifier
 WITH
@@ -66,14 +80,14 @@ LABEL class
 INTO sqlflow_models.my_dnn_model;`)
 	a.NoError(e)
 
-	predParse, e := parser.LegacyParse(`SELECT gender, tenure, TotalCharges
+	predParse, e := parseOneStatement(`SELECT gender, tenure, TotalCharges
 FROM churn.train LIMIT 10
 TO PREDICT iris.predict.class
 USING sqlflow_models.my_dnn_model;`)
 	a.NoError(e)
 	a.NoError(VerifyColumnNameAndType(trainParse, predParse, database.GetTestingDBSingleton()))
 
-	predParse, e = parser.LegacyParse(`SELECT gender, tenure
+	predParse, e = parseOneStatement(`SELECT gender, tenure
 FROM churn.train LIMIT 10
 TO PREDICT iris.predict.class
 USING sqlflow_models.my_dnn_model;`)
@@ -84,10 +98,6 @@ USING sqlflow_models.my_dnn_model;`)
 
 func TestDescribeEmptyTables(t *testing.T) {
 	a := assert.New(t)
-	r, e := parser.LegacyParse(`SELECT * FROM iris.iris_empty LIMIT 10;`)
-	a.NoError(e)
-	_, e = Verify(r.StandardSelect.String(), database.GetTestingDBSingleton())
-	a.EqualError(e, `query SELECT *
-FROM iris.iris_empty
-LIMIT 10 gives 0 row`)
+	_, e := Verify(`SELECT * FROM iris.iris_empty LIMIT 10;`, database.GetTestingDBSingleton())
+	a.EqualError(e, `query SELECT * FROM iris.iris_empty LIMIT 10; gives 0 row`)
 }
