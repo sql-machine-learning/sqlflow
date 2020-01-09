df85137f859f2d606b26f7fa3c7a51e914ba9ab4
shendiaomo <shendiaomo@gmail.com>
2020-01-09 10:59:07 +0800

    Upgrade TensorFlow (#1644)
    * Upgrade TensorFlow
    
    * Update sqlflow_models to the newest version at the moment to upgrade TensorFlow from 2.0.0b1 to 2.0.0
    
    * Fix omitted reshape
    
    * Rewrite binary classification code to be elegant and precise

--- scripts/docker/install-python.bash
+++ scripts/docker/install-python.bash
@@ -30,8 +30,8 @@ apt-get install -y build-essential  libssl-dev # for building mysqlclient pip
 # NOTE: shap == 0.30.1 depends on dill but not include dill as it's dependency, need to install manually.
 # NOTE: mysqlclient depends on apt-get install mysqlclient in install-mysql.bash.
 pip install \
-numpy==1.16.1 \
-tensorflow==2.0.0b1 \
+numpy==1.16.2 \
+tensorflow==2.0.0 \
 mysqlclient==1.4.4 \
 impyla==0.16.0 \
 pyodps==0.8.3 \
