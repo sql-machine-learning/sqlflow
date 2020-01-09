ca5c61b4c712604e28c7b9c32b92d258bf8ef976
Yang Yang(Tony) <29932814+tonyyang-svail@users.noreply.github.com>
2020-01-09 16:39:16 +0800

    Fix CI Maven build (#1667)
    * add mirror in the settings
    
    * multiple build runs
    
    * enable Maven settings.xml
    
    * pressure test
    
    * clean up

--- scripts/docker/install-java.bash
+++ scripts/docker/install-java.bash
@@ -17,3 +17,17 @@ set -e
 
 apt-get -qq update && apt-get install -y openjdk-8-jdk maven > /dev/null
 
+# Use GCS based maven-central mirror.
+# Travis CI occasionally fails on the default maven central repo.
+# Ref: https://github.com/sql-machine-learning/sqlflow/issues/1654
+mkdir -p /root/.m2/
+echo '<settings>
+  <mirrors>
+    <mirror>
+      <id>google-maven-central</id>
+      <name>GCS Maven Central mirror</name>
+      <url>https://maven-central.storage-download.googleapis.com/maven2/</url>
+      <mirrorOf>central</mirrorOf>
+    </mirror>
+  </mirrors>
+</settings>' > /root/.m2/settings.xml
