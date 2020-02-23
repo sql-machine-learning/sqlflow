2c73a16567b048b2a94bd329e363b5b9f8b34a93
Yancey1989 <yancey1989@gmail.com>
2020-02-23 11:34:36 +0800

    fix xgboost validation select

--- python/sqlflow_submitter/xgboost/train.py
+++ python/sqlflow_submitter/xgboost/train.py
@@ -72,13 +72,14 @@ def train(datasource,
     watchlist = [(dtrain, "train")]
 
     if len(validation_select.strip()) > 0:
-        dvalidate = xgb_dataset(datasource, 'validate.txt', select,
+        dvalidate = xgb_dataset(datasource, 'validate.txt', validation_select,
                                 feature_metas, feature_column_names,
                                 label_meta, is_pai, pai_validate_table)
         watchlist.append((dvalidate, "validate"))
 
     re = dict()
     print("Start training XGBoost model...")
+    print("model params: ", model_params)
     bst = xgb.train(model_params,
                     dtrain,
                     evals=watchlist,
