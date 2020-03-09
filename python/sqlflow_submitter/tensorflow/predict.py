278bd2482485f0dafa0a2a3f715733f050f0c100
typhoonzero <typhoonzero1986@gmail.com>
2020-03-09 22:09:23 +0800

    fix scarlar int field used as category id column when predicting

--- python/sqlflow_submitter/tensorflow/predict.py
+++ python/sqlflow_submitter/tensorflow/predict.py
@@ -127,9 +127,9 @@ def keras_predict(estimator, model_params, save, result_table, is_pai,
 
 
 def estimator_predict(estimator, model_params, save, result_table,
-                      feature_column_names, feature_metas, result_col_name,
-                      datasource, select, hdfs_namenode_addr, hive_location,
-                      hdfs_user, hdfs_pass, is_pai, pai_table):
+                      feature_column_names, feature_columns, feature_metas,
+                      result_col_name, datasource, select, hdfs_namenode_addr,
+                      hive_location, hdfs_user, hdfs_pass, is_pai, pai_table):
     if not is_pai:
         conn = db.connect_with_data_source(datasource)
 
@@ -183,14 +183,28 @@ def estimator_predict(estimator, model_params, save, result_table,
                 example.features.feature[feature_name].int64_list.value.extend(
                     list(values))
         else:
+            idx = feature_column_names.index(feature_name)
+            if "feature_columns" in feature_columns:
+                fc = feature_columns["feature_columns"][idx]
+            else:
+                # FIXME(typhoonzero): when the model is deep and wide model, need to
+                # find the feature column form column name, use type numeric_column for now.
+                fc = tf.feature_column.numeric_column("tmp")
             if dtype_str == "float32" or dtype_str == "float64":
                 # need to pass a tuple(float, )
                 example.features.feature[feature_name].float_list.value.extend(
                     (float(x[0][i][0]), ))
             elif dtype_str == "int32" or dtype_str == "int64":
                 # FIXME(typhoonzero): figure out why int64 features need to convert to float
-                example.features.feature[feature_name].float_list.value.extend(
-                    (float(x[0][i][0]), ))
+                numeric_type = type(tf.feature_column.numeric_column("tmp"))
+                if type(fc) == numeric_type:
+                    example.features.feature[
+                        feature_name].float_list.value.extend(
+                            (float(x[0][i][0]), ))
+                else:
+                    example.features.feature[
+                        feature_name].int64_list.value.extend(
+                            (int(x[0][i][0]), ))
             elif dtype_str == "string":
                 example.features.feature[feature_name].bytes_list.value.extend(
                     x[0][i])
@@ -268,9 +282,9 @@ def pred(datasource,
             model_params['model_dir'] = save
         print("Start predicting using estimator model...")
         estimator_predict(estimator, model_params, save, result_table,
-                          feature_column_names, feature_metas, result_col_name,
-                          datasource, select, hdfs_namenode_addr,
-                          hive_location, hdfs_user, hdfs_pass, is_pai,
-                          pai_table)
+                          feature_column_names, feature_columns, feature_metas,
+                          result_col_name, datasource, select,
+                          hdfs_namenode_addr, hive_location, hdfs_user,
+                          hdfs_pass, is_pai, pai_table)
 
     print("Done predicting. Predict table : %s" % result_table)
