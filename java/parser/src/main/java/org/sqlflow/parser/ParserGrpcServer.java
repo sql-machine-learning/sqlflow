3d2981e4ab9a3a11b6c00daa19e13bee12f2878c
typhoonzero <typhoonzero1986@gmail.com>
2020-03-17 21:35:37 +0800

    return parse result

--- java/parser/src/main/java/org/sqlflow/parser/ParserGrpcServer.java
+++ java/parser/src/main/java/org/sqlflow/parser/ParserGrpcServer.java
@@ -79,6 +79,7 @@ public class ParserGrpcServer {
       responseBuilder.addAllSqlStatements(parseResult.statements);
       responseBuilder.setIndex(parseResult.position);
       responseBuilder.setError(parseResult.error);
+      responseBuilder.addAllInputOutputTables(parseResult.inputOutputTables)
 
       responseObserver.onNext(responseBuilder.build());
       responseObserver.onCompleted();
