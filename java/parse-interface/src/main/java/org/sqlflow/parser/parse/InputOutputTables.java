01d9b3a2771a4e9c194cfbec6ebde09fe6357540
typhoonzero <typhoonzero1986@gmail.com>
2020-03-17 16:57:45 +0800

    add files

---
+++ java/parse-interface/src/main/java/org/sqlflow/parser/parse/InputOutputTables.java
@@ -0,0 +1,8 @@
+package org.sqlflow.parser.parse;
+
+import java.util.ArrayList;
+
+public class InputOutputTables {
+  public ArrayList<String> inputTables;
+  public ArrayList<String> outputTables;
+}
