36ed5872368b3877c5d92b9c4d73d2e3ac74cb75
linkerzhang <linkerzhang@yeah.net>
2020-03-26 12:56:33 +0800

    adding one option for populating data locally.

--- scripts/start.sh
+++ scripts/start.sh
@@ -23,7 +23,15 @@ function sleep_until_mysql_is_ready() {
   done
 }
 
+function start_mysql() {
+  # Start mysqld
+  sed -i "s/.*bind-address.*/bind-address = ${SQLFLOW_MYSQL_HOST}/" /etc/mysql/mysql.conf.d/mysqld.cnf
+  service mysql start
+  sleep 1
+}
+
 function setup_mysql() {
+  start_mysql
   populate_example_dataset
   # Grant all privileges to all the remote hosts so that the sqlflow server can
   # be scaled to more than one replicas.
@@ -32,10 +40,6 @@ function setup_mysql() {
 }
 
 function populate_example_dataset() {
-  # Start mysqld
-  sed -i "s/.*bind-address.*/bind-address = ${SQLFLOW_MYSQL_HOST}/" /etc/mysql/mysql.conf.d/mysqld.cnf
-  service mysql start
-  sleep 1
   sleep_until_mysql_is_ready
   # FIXME(typhoonzero): should let docker-entrypoint.sh do this work
   for f in /docker-entrypoint-initdb.d/*; do
@@ -76,6 +80,10 @@ function main() {
       sleep infinity
       ;;
     populate-example-dataset-mysql)
+      populate_example_dataset
+      ;;
+    populate-example-dataset-mysql-local)
+      start_mysql
       populate_example_dataset
       ;;
     sqlflow-server)
