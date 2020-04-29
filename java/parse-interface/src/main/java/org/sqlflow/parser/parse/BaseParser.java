fab154f2396bc1c6bcc02691c96d9290bf61d4e1
HongwuLin <lhw362950217@sina.com>
2020-04-28 20:01:11 -0500

    change BaseParser to accept more parse result fields (#2166)

--- java/parse-interface/src/main/java/org/sqlflow/parser/parse/BaseParser.java
+++ java/parse-interface/src/main/java/org/sqlflow/parser/parse/BaseParser.java
@@ -10,11 +10,12 @@ public abstract class BaseParser implements ParseInterface {
    * parse one statement and return where error occurs.
    *
    * @param statement contains only one sql statement
-   * @return -1 on successfully parse the statement,
-   *   otherwise return where error occurs
+   * @param stmtResult parse result of this statement, input/ouput tables will be merged in final
+   *     result
+   * @return -1 on successfully parse the statement, otherwise return where error occurs
    * @throws Exception means impossible to parse
    */
-  protected abstract int parseOneStmt(String statement) throws Exception;
+  protected abstract int parseOneStmt(String statement, ParseResult stmtResult) throws Exception;
 
   /**
    * is given statement a selection statement.
@@ -25,8 +26,7 @@ public abstract class BaseParser implements ParseInterface {
   protected abstract boolean isSelectStmt(String statement);
 
   /**
-   * split sql into statements, do not trim the result, 
-   * we will calculate length base on the result.
+   * split sql into statements, do not trim the result, we will calculate length base on the result.
    *
    * @param sql is a program with multiple statements
    * @return pieces each is a statement
@@ -45,6 +45,7 @@ public abstract class BaseParser implements ParseInterface {
   public ParseResult parse(String sql) {
     ParseResult parseResult = new ParseResult();
     parseResult.statements = new ArrayList<String>();
+    parseResult.inputOutputTables = new ArrayList<>();
     parseResult.position = 0;
     parseResult.error = "";
     parseResult.isUnfinishedSelect = false;
@@ -54,25 +55,30 @@ public abstract class BaseParser implements ParseInterface {
       List<String> stmts = this.splitStatements(sql);
       for (int i = 0; i < stmts.size(); ++i) {
         String stmt = stmts.get(i);
-        int pos = parseOneStmt(stmt);
+        ParseResult stmtResult = new ParseResult();
+        int pos = parseOneStmt(stmt, stmtResult);
         if (pos == 0) { // error at the first word
           noErr = false;
           break;
         } else if (pos < 0) { // accepted
           parseResult.statements.add(stmt);
           parseResult.position += stmt.length();
+          mergeResult(parseResult, stmtResult);
         } else if (pos == stmt.length() - 1 && stmt.charAt(pos) == ';') {
           // some parser do not accept the last ';'
           parseResult.statements.add(stmt.substring(0, pos));
           parseResult.position += stmt.length();
+          mergeResult(parseResult, stmtResult);
         } else { // error at pos
           noErr = false;
           String prefix = stmt.substring(0, pos);
-          if (parseOneStmt(prefix) < 0) {
+          stmtResult = new ParseResult();
+          if (parseOneStmt(prefix, stmtResult) < 0) {
             if (isSelectStmt(prefix)) {
               parseResult.isUnfinishedSelect = true;
               parseResult.statements.add(prefix);
               parseResult.position += prefix.length();
+              mergeResult(parseResult, stmtResult);
             }
             // else return current parsed stmts
           }
@@ -96,6 +102,12 @@ public abstract class BaseParser implements ParseInterface {
       parseResult.position += getLeadingCommentLen(unparsed);
     }
     return parseResult;
+  }
+
+  private void mergeResult(ParseResult parseResult, ParseResult stmtResult) {
+    if (stmtResult.inputOutputTables != null) {
+      parseResult.inputOutputTables.addAll(stmtResult.inputOutputTables);
+    }
   }
 
   protected static int posToIndex(String query, int line, int column) {
