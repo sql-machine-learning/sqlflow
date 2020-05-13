5eb21628f8f4b682fc48a3a663661343d684d98e
Yi Wang <yi.wang.2005@gmail.com>
2020-05-13 02:37:50 -0700

    Explicit sk

--- scripts/travis/deploy_cli_macos.sh
+++ scripts/travis/deploy_cli_macos.sh
@@ -37,9 +37,10 @@ curl -s "https://awscli.amazonaws.com/AWSCLIV2.pkg" -o "AWSCLIV2.pkg"
 sudo installer -pkg AWSCLIV2.pkg -target /
 
 echo "Publish /tmp/sqlflow to the AWS S3 ..."
+export AWS_ACCESS_KEY_ID=AKIAWTUGQ73KQLDMRTXM
+export AWS_SECRET_ACCESS_KEY=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 aws
 aws --region ap-east-1 --output text \
     s3 cp /tmp/sqlflow s3://sqlflow-release/latest/macos/sqlflow
-
 aws --region ap-east-1 --output text \
     s3api put-object-acl \
     --bucket sqlflow-release \
