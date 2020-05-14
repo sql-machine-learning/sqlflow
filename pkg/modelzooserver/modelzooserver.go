5aaa320fdef369601f564189b67442eadf3619da
Wu Yi <xiongmu.wy@antfin.com>
2020-05-14 17:25:36 +0800

    Release model definitions and list them (#2232)
    * release model definitions and list them
    
    * fix ci
    
    * fix ci

--- pkg/modelzooserver/modelzooserver.go
+++ pkg/modelzooserver/modelzooserver.go
@@ -15,15 +15,82 @@ package modelzooserver
 
 import (
 	"context"
+	"fmt"
+	"io"
+	"log"
 
+	"sqlflow.org/sqlflow/pkg/database"
 	pb "sqlflow.org/sqlflow/pkg/proto"
 )
 
+const modelCollTable = "sqlflow_model_zoo.model_collection"
+const modelDefTable = "sqlflow_model_zoo.model_definition"
+const trainedModelTable = "sqlflow_model_zoo.trained_model"
+
+// TODO(typhoonzero): create tables if these tables are not pre created?
+const createTableStmts = `CREATE DATABASE IF NOT EXISTS sqlflow_model_zoo;
+DROP TABLE IF EXISTS sqlflow_model_zoo.trained_model;
+DROP TABLE IF EXISTS sqlflow_model_zoo.model_definition;
+DROP TABLE IF EXISTS sqlflow_model_zoo.model_collection;
+
+CREATE TABLE sqlflow_model_zoo.model_collection (
+    id INT AUTO_INCREMENT,
+    name VARCHAR(255),
+    version VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE sqlflow_model_zoo.model_definition (
+    id INT AUTO_INCREMENT,
+    model_coll_id INT,
+    class_name VARCHAR(255),
+    args_desc VARCHAR(255),
+    PRIMARY KEY (id),
+    FOREIGN KEY (model_coll_id) REFERENCES model_collection(id)
+);
+
+CREATE TABLE sqlflow_model_zoo.trained_model (
+    id INT AUTO_INCREMENT,
+    model_def_id INT,
+    name VARCHAR(255),
+    version VARCHAR(255),
+    url VARCHAR(255),
+    description TEXT,
+    metrics TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (model_def_id) REFERENCES model_definition(id)
+);`
+
 type modelZooServer struct {
+	DB *database.DB
 }
 
 func (s *modelZooServer) ListModelDefs(ctx context.Context, req *pb.ListModelRequest) (*pb.ListModelResponse, error) {
-	return &pb.ListModelResponse{}, nil
+	// TODO(typhoonzero): join model_collection
+	var sql string
+	if req.Size <= 0 {
+		sql = fmt.Sprintf("SELECT class_name, args_desc FROM %s;", modelDefTable)
+	} else {
+		sql = fmt.Sprintf("SELECT class_name, args_desc FROM %s LIMIT %d OFFSET %d;",
+			modelDefTable, req.Size, req.Start)
+	}
+	rows, err := s.DB.Query(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	names := []string{}
+	arglist := []string{}
+	for rows.Next() {
+		n := ""
+		args := ""
+		if err := rows.Scan(&n, &args); err != nil {
+			return nil, err
+		}
+		names = append(names, n)
+		arglist = append(arglist, args)
+	}
+	return &pb.ListModelResponse{Names: names, Tags: arglist, Size: int64(len(names))}, nil
 }
 
 func (s *modelZooServer) ListTrainedModels(ctx context.Context, req *pb.ListModelRequest) (*pb.ListModelResponse, error) {
@@ -31,12 +98,79 @@ func (s *modelZooServer) ListTrainedModels(ctx context.Context, req *pb.ListMode
 }
 
 func (s *modelZooServer) ReleaseModelDef(stream pb.ModelZooServer_ReleaseModelDefServer) error {
-	err := stream.SendAndClose(&pb.ModelResponse{})
-	return err
+	reqName := ""
+	reqTag := ""
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			break
+		}
+		reqName = req.GetName()
+		reqTag = req.GetTag()
+		if err != nil {
+			log.Printf("ReleaseModelDef error %v", err)
+		}
+	}
+	// TODO(typhoonzero): Check the reqName should be of the format:
+	// hub.docker.com/group/mymodel
+	// group/mymodel
+	// mymodel
+
+	// TODO(typhoonzero): validate the uploaded tar contains valid models.
+
+	// write model_collection
+	sql := fmt.Sprintf("INSERT INTO %s (name, version) VALUES ('%s', '%s');", modelCollTable, reqName, reqTag)
+	modelCollInsertRes, err := s.DB.Exec(sql)
+	if err != nil {
+		return err
+	}
+	// TODO(typhoonzero): Get details information from the uploaded tar
+
+	// Write model_definition
+	modelCollID, err := modelCollInsertRes.LastInsertId()
+	if err != nil {
+		return err
+	}
+	sql = fmt.Sprintf("INSERT INTO %s (model_coll_id, class_name, args_desc) VALUES (%d, 'DNNClassifier', '{aaa,bbb}')", modelDefTable, modelCollID)
+	if _, err := s.DB.Exec(sql); err != nil {
+		return err
+	}
+
+	return stream.SendAndClose(&pb.ModelResponse{Success: true, Message: ""})
 }
 
 func (s *modelZooServer) DropModelDef(ctx context.Context, req *pb.ModelDefRequest) (*pb.ModelResponse, error) {
-	return &pb.ModelResponse{}, nil
+	// 1. find model collection id
+	// TODO(typhoonzero): verify request strings to avoid SQL injection
+	sql := fmt.Sprintf("SELECT id FROM %s WHERE name='%s' and version='%s';",
+		modelCollTable, req.GetName(), req.GetTag())
+	rows, err := s.DB.Query(sql)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var id int
+	end := rows.Next()
+	if !end {
+		return nil, fmt.Errorf("no model collection %s found", req.GetName())
+	}
+	err = rows.Scan(&id)
+	if err != nil {
+		return nil, err
+	}
+	// 2. delete all recoreds in model_definition that have the model_coll_id
+	sql = fmt.Sprintf("DELETE FROM %s WHERE model_coll_id=%d;", modelDefTable, id)
+	_, err = s.DB.Exec(sql)
+	if err != nil {
+		return nil, err
+	}
+	// 3. delete model collection record
+	sql = fmt.Sprintf("DELETE FROM %s WHERE id=%d;", modelCollTable, id)
+	_, err = s.DB.Exec(sql)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.ModelResponse{Success: true, Message: ""}, nil
 }
 
 func (s *modelZooServer) ReleaseTrainedModel(stream pb.ModelZooServer_ReleaseTrainedModelServer) error {
--- pkg/modelzooserver/modelzooserver_test.go
+++ pkg/modelzooserver/modelzooserver_test.go
@@ -18,9 +18,12 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"google.golang.org/grpc"
+	"sqlflow.org/sqlflow/pkg/database"
 	pb "sqlflow.org/sqlflow/pkg/proto"
 	"sqlflow.org/sqlflow/pkg/server"
 )
@@ -31,12 +34,29 @@ func startServer() {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	grpcServer := grpc.NewServer()
-	pb.RegisterModelZooServerServer(grpcServer, &modelZooServer{})
+	mysqlConn, err := database.OpenAndConnectDB("mysql://root:root@tcp(localhost:3306)/?maxAllowedPacket=0")
+	if err != nil {
+		log.Fatalf("failed to connect to mysql: %v", err)
+	}
+	splitedStmts := strings.Split(createTableStmts, ";")
+	for idx, stmt := range splitedStmts {
+		if idx == len(splitedStmts)-2 {
+			// the last stmt is empty
+			break
+		}
+		_, err = mysqlConn.Exec(stmt)
+		if err != nil {
+			log.Fatalf("failed to create model zoo tables: %v", err)
+		}
+	}
+
+	pb.RegisterModelZooServerServer(grpcServer, &modelZooServer{DB: mysqlConn})
 
 	grpcServer.Serve(lis)
 }
 
 func TestModelZooServer(t *testing.T) {
+	a := assert.New(t)
 	go startServer()
 	server.WaitPortReady("localhost:50055", 0)
 
@@ -47,11 +67,24 @@ func TestModelZooServer(t *testing.T) {
 	defer conn.Close()
 
 	client := pb.NewModelZooServerClient(conn)
+
+	stream, err := client.ReleaseModelDef(context.Background())
+	a.NoError(err)
+	modelDefReq := &pb.ModelDefRequest{Name: "hub.docker.com/group/mymodel", Tag: "v0.1"}
+	err = stream.Send(modelDefReq)
+	a.NoError(err)
+	reply, err := stream.CloseAndRecv()
+	a.NoError(err)
+	a.Equal(true, reply.Success)
+
 	res, err := client.ListModelDefs(context.Background(), &pb.ListModelRequest{Start: 0, Size: -1})
-	if err != nil {
-		t.Fatalf("call ListModelDefs error: %v", err)
-	}
-	if res == nil {
-		t.Errorf("res should not be nil")
-	}
+	a.NoError(err)
+	a.Equal(1, len(res.Names))
+
+	_, err = client.DropModelDef(context.Background(), modelDefReq)
+	a.NoError(err)
+
+	res, err = client.ListModelDefs(context.Background(), &pb.ListModelRequest{Start: 0, Size: -1})
+	a.NoError(err)
+	a.Equal(0, len(res.Names))
 }
