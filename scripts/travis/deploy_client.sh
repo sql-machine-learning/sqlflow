2269342386765a904907d420b8eb596e06dfdfbe
Yi Wang <yi.wang.2005@gmail.com>
2020-05-15 12:06:32 -0700

    Enable deploy from pull request for debug

--- scripts/travis/deploy_client.sh
+++ scripts/travis/deploy_client.sh
@@ -19,25 +19,30 @@ set -e
 # For more informaiton about deployment with Travis CI, please refer
 # to the file header of deploy_docker.sh
 
-if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
-    echo "Skip deployment on pull request"
-    exit 0
-fi
+# DEBUG
+#
+# if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+#     echo "Skip deployment on pull request"
+#     exit 0
+# fi
 
-# Figure out the tag to push sqlflow:ci.
-if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
-    if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
-        RELEASE_TAG="nightly"
-    else
-        RELEASE_TAG="latest"
-    fi
-elif [[ "$TRAVIS_TAG" != "" ]]; then
-    RELEASE_TAG="$TRAVIS_TAG"
-else
-    echo "Cannot figure out Docker image tag."
-    exit 1
-fi
+# DEBUG
+#
+# # Figure out the tag to push sqlflow:ci.
+# if [[ "$TRAVIS_BRANCH" == "develop" ]]; then
+#     if [[ "$TRAVIS_EVENT_TYPE" == "cron" ]]; then
+#         RELEASE_TAG="nightly"
+#     else
+#         RELEASE_TAG="latest"
+#     fi
+# elif [[ "$TRAVIS_TAG" != "" ]]; then
+#     RELEASE_TAG="$TRAVIS_TAG"
+# else
+#     echo "Cannot figure out Docker image tag."
+#     exit 1
+# fi
 
+RELEASE_TAG="latest"  # DEBUG
 
 echo "Verify Go is installed ..."
 go env
