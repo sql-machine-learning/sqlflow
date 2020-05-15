92493a12b223d105f09f58b0a79548843227b1e2
Yancey1989 <yancey1989@gmail.com>
2020-05-15 23:41:50 +0800

    update

--- python/sqlflow_submitter/db.py
+++ python/sqlflow_submitter/db.py
@@ -174,7 +174,7 @@ def read_feature(raw_val, feature_spec, feature_name):
 
 
 def selected_cols(driver, conn, select):
-    limited = re.findall("LIMIT [0-9]*$", select.strip())
+    limited = re.findall("LIMIT [0-9]*$", select.upper().strip())
     if not limited:
         select += " LIMIT 1"
     if driver == "hive":
