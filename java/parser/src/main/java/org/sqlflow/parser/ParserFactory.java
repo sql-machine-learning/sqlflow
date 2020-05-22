ef44d09c199e5ead037e9fc1d42a7c1d074d8955
HongwuLin <lhw362950217@sina.com>
2020-05-22 16:54:06 +0800

    use alternative odps,calcite parsers for maxcompute

--- java/parser/src/main/java/org/sqlflow/parser/ParserFactory.java
+++ java/parser/src/main/java/org/sqlflow/parser/ParserFactory.java
@@ -28,7 +28,10 @@ class ParserFactory {
     for (File file : files) {
       String pathToJar = file.getAbsolutePath();
       URL[] urls = {new URL("jar:file:" + pathToJar + "!/")};
-      URLClassLoader cl = URLClassLoader.newInstance(urls);
+      ClassLoader parent = Thread.currentThread().getContextClassLoader();
+      URLClassLoader cl = URLClassLoader.newInstance(urls, parent);
+      // make classes loaded visible to current thread
+      Thread.currentThread().setContextClassLoader(cl);
 
       try (JarFile jarFile = new JarFile(pathToJar)) {
         Enumeration<JarEntry> e = jarFile.entries();
@@ -38,12 +41,11 @@ class ParserFactory {
             continue;
           }
           // -6 because of .class
-          String className = je.getName().substring(0, je.getName().length()
-              - 6);
+          String className = je.getName().substring(0, je.getName().length() - 6);
           className = className.replace('/', '.');
 
-          if (className.startsWith("org.sqlflow") && !className.startsWith(
-              "org.sqlflow.parser.parse")) {
+          if (className.startsWith("org.sqlflow")
+              && !className.startsWith("org.sqlflow.parser.parse")) {
             Class<?> c = cl.loadClass(className);
             if (ParseInterface.class.isAssignableFrom(c)) {
               Object inst = c.getConstructor().newInstance();
@@ -58,8 +60,24 @@ class ParserFactory {
     }
   }
 
+  Class<?> getAlternativeParsers(String... alts) {
+    for (String alt : alts) {
+      if (parsers.containsKey(alt)) {
+        return parsers.get(alt);
+      }
+    }
+    return null;
+  }
+
   public ParseInterface newParser(String dialect) throws Exception {
-    Class<?> c = parsers.get(dialect);
+    Class<?> c = null;
+    if ("maxcompute".equals(dialect)) {
+      // for maxcompute: chose odps or calcite according
+      // to which exists in classpath
+      c = this.getAlternativeParsers(dialect, "odps", "calcite");
+    } else {
+      c = this.getAlternativeParsers(dialect);
+    }
     if (c == null) {
       throw new Exception("parser \"" + dialect + "\" not found");
     }
--- java/parser/src/test/java/org/sqlflow/parser/ParserFactoryTest.java
+++ java/parser/src/test/java/org/sqlflow/parser/ParserFactoryTest.java
@@ -1,8 +1,10 @@
 package org.sqlflow.parser;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
+import org.sqlflow.parser.parse.ParseInterface;
 
 public class ParserFactoryTest {
   @Test
@@ -10,7 +12,23 @@ public class ParserFactoryTest {
     String folderPath = System.getenv("SQLFLOW_PARSER_SERVER_LOADING_PATH");
     assertNotNull(folderPath);
     ParserFactory parserFactory = new ParserFactory(folderPath);
-    parserFactory.newParser("hive");
-    parserFactory.newParser("calcite");
+    assertEquals("hive", parserFactory.newParser("hive").dialect());
+    assertEquals("calcite", parserFactory.newParser("calcite").dialect());
+  }
+
+  @Test
+  public void testMaxcompute() throws Exception {
+    String folderPath = System.getenv("SQLFLOW_PARSER_SERVER_LOADING_PATH");
+    assertNotNull(folderPath);
+    ParserFactory parserFactory = new ParserFactory(folderPath);
+    try {
+      ClassLoader cl = Thread.currentThread().getContextClassLoader();
+      cl.loadClass("org.sqlflow.parser.OdpsParserAdaptor");
+      ParseInterface mcParaser = parserFactory.newParser("maxcompute");
+      assertEquals("odps", mcParaser.dialect());
+    } catch (ClassNotFoundException e) {
+      ParseInterface mcParaser = parserFactory.newParser("maxcompute");
+      assertEquals("calcite", mcParaser.dialect());
+    }
   }
 }
