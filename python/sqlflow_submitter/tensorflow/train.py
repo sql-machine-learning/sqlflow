1a3c7fa95932ea4603bf907db27651a3c0176bbd
typhoonzero <typhoonzero1986@gmail.com>
2020-05-28 10:17:27 +0800

    fix import any package name

--- python/sqlflow_submitter/tensorflow/train.py
+++ python/sqlflow_submitter/tensorflow/train.py
@@ -70,7 +70,7 @@ def train(datasource,
           pai_table="",
           pai_val_table=""):
     # import custom model package
-    model_import_name = sqlflow_submitter.import_model_def(estimator_string)
+    sqlflow_submitter.import_model_def(estimator_string)
     estimator = eval(estimator_string)
 
     if isinstance(estimator, types.FunctionType):
