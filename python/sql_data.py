511d6ad07e43a8e29c2548b83772e2502864426b
lzj000 <1018473600@qq.com>
2020-06-01 00:05:42 +0800

    Add interface notes (#2391)
    Co-authored-by: lzj000 <1018473600@163.com>

--- python/sql_data.py
+++ python/sql_data.py
@@ -19,6 +19,11 @@ def connect(user, passwd, host, port):
     """connect is a convenient shortcut to mysql.connector.connect. Also,
     it makes it reasonable to import mysql.connector in this file, so
     to make it self-complete as a template.
+    Args:
+        user: Specifies the MySQL user name.
+        passwd : Specify the MySQL password.
+        host : The host name or IP address.
+        port : Specifies the port number that attempts to connect to the MySQL server.
 
     """
     return connect(user=user, passwd=passwd, host=host, port=port)
