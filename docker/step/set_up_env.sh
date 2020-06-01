4c173e74745f43afeff207706a8458489ff8e79b
HongwuLin <lhw362950217@sina.com>
2020-06-01 23:37:01 +0800

    add copyleft

--- docker/step/set_up_env.sh
+++ docker/step/set_up_env.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 . /find_fastest_resources.sh
 
 echo "Find fastest apt-get mirror ..."
