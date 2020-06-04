23fe004037f5e3522a5c2ac289504de68aa5e37c
typhoonzero <typhoonzero1986@gmail.com>
2020-06-04 22:40:00 +0800

    WIP: python api for model developers

---
+++ python/sqlflow_submitter/api/train.py
@@ -0,0 +1,35 @@
+# Copyright 2020 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from sqlflow_submitter import tensorflow
+
+from .. import db
+from . import API_DB_CONN_STR
+
+
+def train(sql, model, into, attrs={}, columns=[], validation_select=""):
+    model_type = "tf"
+    if model.startswith("xgboost"):
+        model_type = "xgboost"
+
+    # derive feature columns from sql statement and columns settings.
+    conn = db.connect_with_data_source(API_DB_CONN_STR)
+    cur = conn.cursor()
+    cur.execute(sql)
+    field_names = []
+    for desc in cur.description:
+        field_names.append(desc[0])
+        # get field types
+
+    # if model_type == "tf":
+    #     tensorflow.train.train(API_DB_CONN_STR, model, sql, validation_select)
