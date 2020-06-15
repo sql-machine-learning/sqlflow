45ec301a3b14eede2541b2d30eb477bbcc354ec3
Yancey1989 <yancey1989@gmail.com>
2020-06-15 09:16:04 +0800

    debug setup argo on ci

--- scripts/travis/start_argo.sh
+++ scripts/travis/start_argo.sh
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -ex
 
 # If argo already installed, skip
 if kubectl get namespace argo>/dev/null 2>&1; then
