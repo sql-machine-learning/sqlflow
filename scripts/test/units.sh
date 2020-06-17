f7b7da963a85287deab36496f5ccd9a8cfdbdf25
sneaxiy@126.com <sneaxiy@126.com>
2020-06-17 08:49:23 +0800

    test ci 2

--- scripts/test/units.sh
+++ scripts/test/units.sh
@@ -35,9 +35,13 @@ python -c "import sqlflow_submitter.db"
 go generate ./...
 go install ./...
 
+set -ex
+
+pip list
+
 # -p 1 is necessary since tests in different packages are sharing the same database
 # ref: https://stackoverflow.com/a/23840896
 # set test timeout to 900s since travis CI may be slow to run the case TestParse
-gotest -v -p 1 -timeout 900s ./...  -covermode=count -coverprofile=coverage.txt
+# gotest -v -p 1 -timeout 900s ./...  -covermode=count -coverprofile=coverage.txt
 
-python -m unittest discover -v python "*_test.py"
+# python -m unittest discover -v python "*_test.py"
