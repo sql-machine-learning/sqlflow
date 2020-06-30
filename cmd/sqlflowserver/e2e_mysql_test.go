772bfb96643b3a84115ffbf9857260f0b88723c2
sneaxiy <sneaxiy@126.com>
2020-06-30 06:43:07 +0000

    make combiner optional

--- cmd/sqlflowserver/e2e_mysql_test.go
+++ cmd/sqlflowserver/e2e_mysql_test.go
@@ -127,7 +127,7 @@ USING sqlflow_models.my_dnn_model;`, // predict using feature derivation model
 		`SELECT c1, c2, c3, c4, c5, class from feature_derivation_case.train
 TO TRAIN DNNClassifier
 WITH model.n_classes=3, model.hidden_units=[10,10]
-COLUMN EMBEDDING(c3, 32, sum), EMBEDDING(SPARSE(c5, 64, COMMA), 32, sum)
+COLUMN EMBEDDING(c3, 32, sum), EMBEDDING(SPARSE(c5, 64, COMMA), 32)
 LABEL class
 INTO sqlflow_models.my_dnn_model;`, // general case to derive all column types
 		`SELECT c1, c2, c3, c4, c5, class from feature_derivation_case.train
