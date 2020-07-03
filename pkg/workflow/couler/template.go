e1cdc9ee28d7a0a482f6a256f779634da2f0c271
HongwuLin <lhw362950217@sina.com>
2020-07-03 19:00:58 +0800

    fix string escape in workflow codegen

--- pkg/workflow/couler/template.go
+++ pkg/workflow/couler/template.go
@@ -69,7 +69,7 @@ couler.clean_workflow_after_seconds_finished({{.WorkflowTTL}})
 {{ range $ss := .SQLStatements }}
 	{{if $ss.IsExtendedSQL }}
 
-steps.sqlflow(sql='''{{ $ss.OriginalSQL }}''', image="{{ $ss.DockerImage }}", env=step_envs, secret=sqlflow_secret, resources=resources)
+steps.sqlflow(sql=r'''{{ $ss.OriginalSQL }}''', image="{{ $ss.DockerImage }}", env=step_envs, secret=sqlflow_secret, resources=resources)
 	{{else if $ss.IsKatibTrain}}
 import couler.sqlflow.katib as auto
 
@@ -81,7 +81,7 @@ auto.train(model=model, params=params, sql=escape_sql(train_sql), datasource=dat
 # TODO(yancey1989): 
 #	using "repl -parse" to output IR and
 #	feed to "sqlflow_submitter.{submitter}.train" to submit the job
-steps.sqlflow(sql='''{{ $ss.OriginalSQL }}''', image="{{ $ss.DockerImage }}", env=step_envs, resources=resources)
+steps.sqlflow(sql=r'''{{ $ss.OriginalSQL }}''', image="{{ $ss.DockerImage }}", env=step_envs, resources=resources)
 	{{end}}
 {{end}}
 `
