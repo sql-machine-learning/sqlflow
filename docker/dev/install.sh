bf84db7c5836caf75275c9b529ee29e592bdeb02
typhoonzero <typhoonzero1986@gmail.com>
2020-07-07 14:59:25 +0800

    test

--- docker/dev/install.sh
+++ docker/dev/install.sh
@@ -85,7 +85,8 @@ export PATH="/usr/local/go/bin:$GOPATH/bin:$PATH"
 echo "Install goyacc, protoc-gen-go, linters, etc. ..."
 # Set the env system-wide for later usage, e.g. build source
 go env -w GO111MODULE=on
-go env -w GOPROXY="$(find_fastest_go_proxy)"
+# go env -w GOPROXY="$(find_fastest_go_proxy)"
+go env -w GOPROXY="https://goproxy.io"
 go get \
    github.com/golang/protobuf/protoc-gen-go@v1.3.3 \
    golang.org/x/lint/golint \
@@ -119,11 +120,11 @@ axel --quiet --output /usr/local/bin/protoc-gen-grpc-java \
 chmod +x /usr/local/bin/protoc-gen-grpc-java
 
 
-echo "Choose fastest Maven mirror ..."
+# echo "Choose fastest Maven mirror ..."
 # Travis CI occasionally fails on the default Maven central repo.
 # Ref: https://github.com/sql-machine-learning/sqlflow/issues/1654
-mkdir -p "$HOME/.m2"
-find_fastest_maven_repo >"$HOME/.m2/settings.xml"
+# mkdir -p "$HOME/.m2"
+# find_fastest_maven_repo >"$HOME/.m2/settings.xml"
 
 
 echo "Install Java linter ..."
