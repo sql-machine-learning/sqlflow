8c3fbe3e978c7f30dabf446150070579e26958a8
Zeng Jinle <32832641+sneaxiy@users.noreply.github.com>
2020-07-17 20:47:31 -0500

    Add Python verifier code (#2706)
    * add Python verifier code
    
    * add python method doc

---
+++ python/runtime/verifier/verifier.py
@@ -0,0 +1,70 @@
+# Copyright 2020 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import runtime.db as db
+
+
+def fetch_samples(conn, query, n=1):
+    '''
+    Fetch n sample(s) at most according to the query statement.
+
+    Args:
+        conn: the connection object.
+        query (str): the select SQL statement.
+        n (int): the maximum sample number to query. Query all samples if n < 0.
+
+    Returns:
+        A generator which yields each row of the data.
+    '''
+
+    query = db.limit_select(query, n)
+    for rows, _ in db.db_generator(conn, query)():
+        yield rows
+
+
+def verify_column_name_and_type(conn, train_select, pred_select, label):
+    '''
+    Verify whether the columns in the SQL statement for prediction
+    contain all columns except the label column in the SQL statement for
+    training. This method would also verify whether the field type are
+    the same for the same columns between the SQL statement for training
+    and prediction.
+
+    Args:
+        conn: the connection object.
+        train_select (str): the select SQL statement for training.
+        pred_select (str): the select SQL statement for prediction.
+        label (str): the label name for training.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: if any column name or type does not match.
+    '''
+    train_schema = db.selected_columns_and_types(conn, train_select)
+    pred_schema = dict(db.selected_columns_and_types(conn, pred_select))
+
+    for name, train_type in train_schema:
+        if name == label:
+            continue
+
+        pred_type = pred_schema.get(name)
+        if pred_type is None:
+            raise ValueError(
+                "the predict statement doesn't contain column %s" % name)
+
+        if pred_type != train_type:
+            raise ValueError(
+                "field %s type dismatch %s(predict) vs %s(train)" %
+                (name, pred_type, train_type))
---
+++ python/runtime/verifier/verifier_test.py
@@ -0,0 +1,116 @@
+# Copyright 2020 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import runtime.db as db
+import runtime.testing as testing
+from runtime.verifier import fetch_samples, verify_column_name_and_type
+
+
+def length(iterable):
+    n = 0
+    for _ in iterable:
+        n += 1
+
+    return n
+
+
+class TestFetchSamples(unittest.TestCase):
+    @unittest.skipUnless(testing.get_driver() in ["mysql", "hive"],
+                         "skip non mysql/hive tests")
+    def test_fetch_sample(self):
+        conn = testing.get_singleton_db_connection()
+
+        select = "SELECT * FROM iris.train"
+        column_num = len(db.selected_columns_and_types(conn, select))
+
+        gen = fetch_samples(conn, select, n=-1)
+        row_num = length(gen)
+        self.assertGreater(row_num, 25)
+
+        gen = fetch_samples(conn, select, n=25)
+        n = 0
+        for rows in gen:
+            self.assertEqual(len(rows), column_num)
+            n += 1
+        self.assertEqual(n, 25)
+
+        gen = fetch_samples(conn, select, n=10)
+        self.assertEqual(length(gen), 10)
+
+        gen = fetch_samples(conn, "%s LIMIT 1" % select, n=1000)
+        self.assertEqual(length(gen), 1)
+
+        gen = fetch_samples(conn, select, n=row_num * 2)
+        self.assertEqual(length(gen), row_num)
+
+
+class TestFetchVerifyColumnNameAndType(unittest.TestCase):
+    def generate_select(self, table, columns):
+        return "SELECT %s FROM %s" % (",".join(columns), table)
+
+    @unittest.skipUnless(testing.get_driver() in ["mysql", "hive"],
+                         "skip non mysql/hive tests")
+    def test_verify_column_name_and_type(self):
+        conn = testing.get_singleton_db_connection()
+
+        train_table = "iris.train"
+        test_table = "iris.test"
+
+        train_select = [
+            "petal_length", "petal_width", "sepal_length", "sepal_width",
+            "class"
+        ]
+        test_select = train_select
+        verify_column_name_and_type(
+            conn, self.generate_select(train_table, train_select),
+            self.generate_select(test_table, test_select), "class")
+
+        test_select = [
+            "petal_length", "petal_width", "sepal_length", "sepal_width"
+        ]
+        verify_column_name_and_type(
+            conn, self.generate_select(train_table, train_select),
+            self.generate_select(test_table, test_select), "class")
+
+        test_select = ["petal_length", "petal_width", "sepal_length"]
+        with self.assertRaises(ValueError):
+            verify_column_name_and_type(
+                conn, self.generate_select(train_table, train_select),
+                self.generate_select(test_table, test_select), "class")
+
+        cursor = conn.cursor()
+        name_and_type = dict(db.get_table_schema(conn, test_table))
+        new_table_name = "iris.verifier_test_table"
+
+        name_and_type["petal_length"] = "VARCHAR(255)"  # change the data type
+        create_column_str = ",".join(
+            ["%s %s" % (n, t) for n, t in name_and_type.items()])
+
+        drop_sql = "DROP TABLE IF EXISTS %s" % new_table_name
+        create_sql = "CREATE TABLE %s(%s)" % (new_table_name,
+                                              create_column_str)
+        cursor.execute(drop_sql)
+        cursor.execute(create_sql)
+        with self.assertRaises(ValueError):
+            test_select = train_select
+            verify_column_name_and_type(
+                conn, self.generate_select(train_table, train_select),
+                self.generate_select(new_table_name, test_select), "class")
+        cursor.execute(drop_sql)
+        cursor.close()
+
+
+if __name__ == "__main__":
+    unittest.main()
