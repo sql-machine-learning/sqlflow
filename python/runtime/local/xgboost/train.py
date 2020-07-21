f8ea5aa2cfaafa0aad6b7479aa8dbb67b407d548
sneaxiy <sneaxiy@126.com>
2020-07-21 06:14:04 +0000

    polish xgboost local train code

--- python/runtime/local/xgboost/train.py
+++ python/runtime/local/xgboost/train.py
@@ -53,16 +53,17 @@ def train(train_dataset,
 
     bst = init_xgb_booster(load_xgb_pretrained_model)
     eval_result = dict()
-    for per_batch_dmatrix in train_dataset:
-        watchlist = [(per_batch_dmatrix, "train")]
-        if val_dataset:
-            # the `xgboost.train` API accepts the XGBoost DMatrix object
-            # as the training or validation dataste, so we should convert
-            # the generator to DMatrix.
-            if isinstance(val_dataset, types.GeneratorType):
-                val_dataset = list(val_dataset)[0]
-            watchlist.append((val_dataset, "validate"))
+    watchlist = [None]
+    if val_dataset:
+        # The `xgboost.train` API only accepts the XGBoost DMatrix
+        # object as the training or validation dataset, so we should
+        # convert the generator to DMatrix.
+        if isinstance(val_dataset, types.GeneratorType):
+            val_dataset = list(val_dataset)[0]
+        watchlist.append((val_dataset, "validate"))
 
+    for per_batch_dmatrix in train_dataset:
+        watchlist[0] = (per_batch_dmatrix, "train")
         bst = xgb.train(model_params,
                         per_batch_dmatrix,
                         evals=watchlist,
