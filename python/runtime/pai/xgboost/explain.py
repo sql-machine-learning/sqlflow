05b6fc0ef90746e76e0f4128a4690f5e923566c5
HongwuLin <lhw362950217@sina.com>
2020-07-23 19:09:30 +0800

    move tensorflow files to a sub-package

---
+++ python/runtime/pai/xgboost/explain.py
@@ -0,0 +1,60 @@
+# Copyright 2020 The SQLFlow Authors. All rights reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License
+
+import json
+import os
+
+import runtime.xgboost as xgboost_extended
+from runtime import oss
+from runtime.pai.pai_distributed import define_tf_flags, set_oss_environs
+from runtime.xgboost.explain import explain as explain_xgb
+
+FLAGS = define_tf_flags()
+
+
+def explain(datasource, select, data_table, result_table, label_column,
+            oss_model_path):
+    # NOTE(typhoonzero): the xgboost model file "my_model" is hard coded in xgboost/train.py
+    oss.load_file(oss_model_path, "my_model")
+
+    (estimator, model_params, train_params, feature_field_meta,
+     feature_column_names, label_field_meta,
+     feature_column_code) = oss.load_metas(oss_model_path,
+                                           "xgboost_model_desc")
+
+    feature_column_transformers = eval('[{}]'.format(feature_column_code))
+    transform_fn = xgboost_extended.feature_column.ComposedColumnTransformer(
+        feature_column_names, *feature_column_transformers)
+
+    explain_xgb(
+        datasource=datasource,
+        select=select,
+        feature_field_meta=feature_field_meta,
+        feature_column_names=feature_column_names,
+        label_meta=label_field_meta,
+        summary_params={},
+        result_table=result_table,
+        is_pai=True,
+        pai_explain_table=data_table,
+        hdfs_namenode_addr="",
+        hive_location="",
+        hdfs_user="",
+        hdfs_pass="",
+        # (TODO:lhw) save/load explain result storage info into/from FLAGS
+        oss_dest="",
+        oss_ak="",
+        oss_sk="",
+        oss_endpoint="",
+        oss_bucket_name="",
+        transform_fn=transform_fn,
+        feature_column_code=feature_column_code)
