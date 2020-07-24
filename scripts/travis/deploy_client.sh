8ef813fdb97a10095e623cdabace4448ac48feff
typhoonzero <typhoonzero1986@gmail.com>
2020-07-24 11:20:23 +0800

    test

--- scripts/travis/deploy_client.sh
+++ scripts/travis/deploy_client.sh
@@ -86,7 +86,10 @@ go get \
    github.com/golang/protobuf/protoc-gen-go@v1.3.3 \
    golang.org/x/tools/cmd/goyacc \
    > /dev/null
+echo "using GOPATH: $GOPATH"
 export PATH=$GOPATH/bin:$PATH
+echo "files under GOPATH:" 
+ls -l "$GOPATH/bin"
 
 
 echo "Build cmd/sqlflow into /tmp ..."
