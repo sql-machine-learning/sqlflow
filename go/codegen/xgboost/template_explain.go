89e1f6e42c3e4a8d611e0ef5aca48c729877fbf4
Wu Yi <typhoonzero1986@gmail.com>
2020-07-30 13:43:23 +0800

    fix xgboost training with greater than 255 feature columns (#2774)

--- go/codegen/xgboost/template_explain.go
+++ go/codegen/xgboost/template_explain.go
@@ -42,7 +42,8 @@ feature_column_names = [{{range .FeatureColumnNames}}
 "{{.}}",
 {{end}}]
 
-transform_fn = xgboost_extended.feature_column.ComposedColumnTransformer(feature_column_names, {{.FeatureColumnCode}})
+feature_column_list = [{{.FeatureColumnCode}}]
+transform_fn = xgboost_extended.feature_column.ComposedColumnTransformer(feature_column_names, *feature_column_list)
 
 explain(
     datasource='''{{.DataSource}}''',
