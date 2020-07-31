0ce71711cd61ef68c9b2eb6c957c88cb20801362
sneaxiy <sneaxiy@126.com>
2020-07-31 00:32:26 +0000

    add more ut

--- python/runtime/optimize/optflow_fsl_generaion_test.py
+++ python/runtime/optimize/optflow_fsl_generaion_test.py
@@ -56,6 +56,59 @@ class TestOptFlowFSLGeneration(unittest.TestCase):
             c_expr, 'for j in @J: sum([@X[i,j] for i in @I])'
             '>=@input["demand"][@I[0],j]')
 
+    def test_asset_case(self):
+        columns = [
+            'user_id', 'org_id', 'max_num', 'k', 'c', 'u', 'p',
+            'asset_at_risk', 'asset'
+        ]
+        variables = ['user_id', 'org_id']
+        result_value_name = 'x'
+
+        obj_tokens = ['SUM', '(', 'k', '*', 'x', ')']
+
+        obj_expr = gen_expr(columns=columns,
+                            tokens=obj_tokens,
+                            variables=variables,
+                            result_value_name=result_value_name)
+        self.assertEqual(
+            obj_expr, 'sum([@input["k"][i,j]*@X[i,j] '
+            'for i in @I for j in @J])')
+
+        constraint_tokens = ['SUM', '(', 'x', ')', '<=', 'max_num']
+        c_expr = gen_expr(columns=columns,
+                          tokens=constraint_tokens,
+                          variables=variables,
+                          result_value_name=result_value_name,
+                          group_by='org_id')
+        self.assertEqual(
+            c_expr, 'for j in @J: sum([@X[i,j] for i in @I])'
+            '<=@input["max_num"][@I[0],j]')
+
+        constraint_tokens = ['SUM', '(', 'x', ')', '=', '1']
+        c_expr = gen_expr(columns=columns,
+                          tokens=constraint_tokens,
+                          variables=variables,
+                          result_value_name=result_value_name,
+                          group_by='user_id')
+        self.assertEqual(c_expr, 'for i in @I: sum([@X[i,j] for j in @J])==1')
+
+        constraint_tokens = [
+            'SUM', '(', 'c', '*', 'u', '*', 'p', '*', 'x', ')', '+',
+            'asset_at_risk', '<=', '0.055', '*', '(', 'SUM', '(', 'c', '*',
+            'u', '*', 'x', ')', '+', 'asset', ')'
+        ]
+        c_expr = gen_expr(columns=columns,
+                          tokens=constraint_tokens,
+                          variables=variables,
+                          result_value_name=result_value_name,
+                          group_by='user_id')
+        self.assertEqual(
+            c_expr, 'for i in @I: sum([@input["c"][i,j]*@input["u"][i,j]*'
+            '@input["p"][i,j]*@X[i,j] for j in @J])+'
+            '@input["asset_at_risk"][i,@J[0]]'
+            '<=0.055*(sum([@input["c"][i,j]*@input["u"][i,j]*@X[i,j] '
+            'for j in @J])+@input["asset"][i,@J[0]])')
+
 
 if __name__ == '__main__':
     unittest.main()
