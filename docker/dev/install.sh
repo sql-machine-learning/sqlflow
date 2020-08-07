04e984e475c9a3f7886e3cce6117fd693f85b78b
typhoonzero <typhoonzero1986@gmail.com>
2020-08-07 09:04:46 +0800

    test install.sh

--- docker/dev/install.sh
+++ docker/dev/install.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -ex
 
 if [ "$FIND_FASTED_MIRROR" == "true" ]; then
     # shellcheck disable=SC1091
