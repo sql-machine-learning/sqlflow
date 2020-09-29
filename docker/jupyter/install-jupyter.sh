0075c274228db41102ffd90d067f4307db1d53b0
Wu Yi <typhoonzero1986@gmail.com>
2020-09-29 10:46:32 +0800

    Test sqlflow ipython magic in ci (#2925)
    * test sqlflow ipython magic in ci
    
    * fix run on ci

--- docker/jupyter/install-jupyter.sh
+++ docker/jupyter/install-jupyter.sh
@@ -29,7 +29,7 @@ apk add py3-cryptography py3-ruamel.yaml.clib py3-requests
 pip -q install \
     jupyterhub==1.1.0 \
     notebook==6.0.3 \
-    sqlflow==0.13.0
+    sqlflow==0.14.0
 
 # Load SQLFlow's Jupyter magic command
 # automatically. c.f. https://stackoverflow.com/a/32683001.
