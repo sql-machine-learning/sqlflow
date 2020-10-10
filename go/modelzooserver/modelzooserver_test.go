daa4221ebeb53040e3b07f34295ccabb4971de70
typhoonzero <typhoonzero1986@gmail.com>
2020-10-10 16:02:28 +0800

    update

--- go/modelzooserver/modelzooserver_test.go
+++ go/modelzooserver/modelzooserver_test.go
@@ -136,7 +136,9 @@ func TestModelZooServer(t *testing.T) {
 		sampleModel := model.New(dir, "SAMPLE TRAIN SELECT")
 		sampleModel.Meta = jsonMeta
 		modelTableName := "sqlflow_models.model_zoo_sample_model"
-		if os.Getenv("SQLFLOW_USE_EXPERIMENTAL_CODEGEN") == "true" {
+		// NOTE(typhoonzero): change to if os.Getenv("SQLFLOW_USE_EXPERIMENTAL_CODEGEN") == "true"
+		// when switch to refactored code.
+		if false {
 			err = sampleModel.SaveDBExperimental(dbConnStr, modelTableName, &pb.Session{
 				DbConnStr: dbConnStr,
 			})
