3f13c3aa4ac2fbd555c002d2b44dd0db5e0f2551
HongwuLin <lhw362950217@sina.com>
2020-10-15 19:16:29 +0800

    format code

--- python/runtime/step/tensorflow/train.py
+++ python/runtime/step/tensorflow/train.py
@@ -80,11 +80,12 @@ def train_step(original_sql,
     feature_column_names = [fd.name for fd in field_descs]
     feature_metas = dict([(fd.name, fd.to_dict(dtype_to_string=True))
                           for fd in field_descs])
-                        
+
     # no label for clustering model
     label_meta = None
     if fc_label_ir:
-        label_meta = fc_label_ir.get_field_desc()[0].to_dict(dtype_to_string=True)
+        label_meta = fc_label_ir.get_field_desc()[0].to_dict(
+            dtype_to_string=True)
 
     feature_column_names_map = dict()
     for target in fc_map_ir:
