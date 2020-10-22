c302e486b1ae5049dd5d2b4becbec55a26810ae1
Wu Yi <typhoonzero1986@gmail.com>
2020-10-22 14:09:47 +0800

    Fix sqlflow:ci image build error with pip deps resolving after 2020.10 (#3049)
    * fix sqlflow:ci image build error with pip deps resolving after 2020.10
    
    * fix image build push
    
    * update
    
    * fix ci

--- docker/dev/build.sh
+++ docker/dev/build.sh
@@ -85,7 +85,7 @@ if [[ ! -d models ]]; then
 fi
 cd models
 git fetch origin # The residual local repo might not be on a branch.
-git checkout 90e3b8b0de91faef942315a7dca7f94d175cec97
+git checkout 36a2f217f3311d861cf736894ecb5147b6e44c74
 python setup.py bdist_wheel -q --dist-dir $SQLFLOW_BIN > /dev/null
 
 echo "Convert tutorials from Markdown to IPython notebooks ..."
