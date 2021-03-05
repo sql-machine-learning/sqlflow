71a62af6219133d7989b799cca6f9c03aa0947d3
typhoonzero <typhoonzero1986@gmail.com>
2021-03-05 14:59:11 +0800

    update

--- go/workflow/couler/template.go
+++ go/workflow/couler/template.go
@@ -129,7 +129,7 @@ couler.run_container(command=["bash", "-c", step_command('''{{ $ss.OriginalSQL}}
 couler.config_workflow(cluster_config_file=cluster_config, time_to_clean=workflow_ttl)
 {{ if .UseCoulerSubmitter }}
 from ant_couler.couler_submitter import CoulerSubmitter
-sbmtr = CoulerSubmitter(address="{{.CoulerServerAddr}}", namespace="kubemaker")
+sbmtr = CoulerSubmitter(address="{{.CoulerServerAddr}}", namespace="sqlflow")
 resp = couler.run(submitter=sbmtr, cluster="{{.CoulerCluster}}")
 print(resp.name)
 {{ end }}
